06ccaa967681c449636f825d9610fef780e02c61
Margarita <kkom.margarita@gmail.com>
2026-06-19 22:01:08 +0500

    feat: update AlarmReceiver to auto-cancel notifications and use activity context for intent flags

--- app/src/main/java/com/example/cleveralarmclock/alarmHandler/AlarmReceiver.kt
+++ app/src/main/java/com/example/cleveralarmclock/alarmHandler/AlarmReceiver.kt
@@ -1,18 +1,19 @@
 package com.example.cleveralarmclock.alarmHandler
 
+import android.annotation.SuppressLint
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
-import android.os.Build
 import android.util.Log
 import androidx.core.app.NotificationCompat
 import com.example.cleveralarmclock.MainActivity
 import com.example.cleveralarmclock.R
 
 class AlarmReceiver : BroadcastReceiver() {
+
 
     override fun onReceive(context: Context?, intent: Intent?) {
         val ctx = context ?: return
@@ -21,10 +22,11 @@ class AlarmReceiver : BroadcastReceiver() {
             // TODO
         }else{
             Log.i("ALARM_DEBUG", "Время пришло! Показываем уведомление звонка.")
-            val startAppIntent = Intent(context, MainActivity::class.java).apply {
+            val startAppIntent = Intent(ctx, MainActivity::class.java).apply {
                 flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
                 putExtra("ALARM_TRIGGERED",true)
             }
+
             val pendingIntent: PendingIntent = PendingIntent.getActivity(
                 context,
                 0,
@@ -32,6 +34,7 @@ class AlarmReceiver : BroadcastReceiver() {
                 PendingIntent.FLAG_IMMUTABLE or PendingIntent.FLAG_UPDATE_CURRENT
             )
             val channelId = "chanelId"
+
             val notificationManager: NotificationManager = context.getSystemService(
                 NotificationManager::class.java) as NotificationManager
             val channel = NotificationChannel(
@@ -50,6 +53,7 @@ class AlarmReceiver : BroadcastReceiver() {
                 .setContentText(ctx.getString(R.string.notification_description))
                 .setPriority(NotificationCompat.PRIORITY_HIGH)
                 .setCategory(NotificationCompat.CATEGORY_ALARM)
+                .setAutoCancel(true)
                 .setFullScreenIntent(pendingIntent, true)
 
             notificationManager.notify(1, builder.build())
