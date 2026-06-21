4522bd9259589e6bf046d600bc48cee9ae6fad02
Margarita <kkom.margarita@gmail.com>
2026-06-21 17:31:10 +0500

    feat: implement MainViewModel in mainScreenFeature for alarm scheduling and cancellation

---
+++ app/src/main/java/com/example/cleveralarmclock/presentation/mainScreenFeature/data/MainViewModel.kt
@@ -0,0 +1,18 @@
+package com.example.cleveralarmclock.presentation.mainScreenFeature.data
+
+import androidx.lifecycle.ViewModel
+import com.example.cleveralarmclock.alarmHandler.AlarmSchedule
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+@HiltViewModel
+class MainViewModel @Inject constructor(
+    val alarmSchedule: AlarmSchedule
+): ViewModel(){
+    fun startAlarm(){
+        alarmSchedule.schedule()
+    }
+
+    fun cancelAlarm(){
+        alarmSchedule.cancel()
+    }
+}
