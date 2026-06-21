f96c6009681de3e4ffad3380eb872943362b5d2a
Margarita <kkom.margarita@gmail.com>
2026-06-21 17:31:03 +0500

    feat: integrate MainViewModel and add start and cancel alarm buttons to MainScreen

--- app/src/main/java/com/example/cleveralarmclock/presentation/mainScreenFeature/MainScreen.kt
+++ app/src/main/java/com/example/cleveralarmclock/presentation/mainScreenFeature/MainScreen.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -15,33 +16,65 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.example.cleveralarmclock.presentation.mainScreenFeature.data.MainViewModel
+
 
 @Composable
-fun MainScreen(modifier: Modifier = Modifier) {
+fun MainScreen(
+    viewModel: MainViewModel = hiltViewModel(),
+    modifier: Modifier = Modifier
+) {
     val listForTest = arrayListOf("item1", "item2")
-    Scaffold (
+    Scaffold(
         modifier = modifier
             .fillMaxSize()
-    ){ innerPadding->
-        LazyColumn (
-            modifier = modifier
+    ) { innerPadding ->
+        Box(
+            modifier = Modifier
                 .fillMaxSize()
-                .padding(innerPadding),
-            horizontalAlignment = Alignment.CenterHorizontally
-        ){
-            items(listForTest){item ->
-                Box (
-                    modifier = modifier
-                        .padding(all = 12.dp)
-                        .background(Color.Blue),
-                    contentAlignment = Alignment.Center
-                ){
-                    Text(item, style = MaterialTheme.typography.titleLarge, color = Color.White)
+                .padding(innerPadding)
+        ) {
+
+            LazyColumn(
+                modifier = Modifier.fillMaxSize(),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                items(listForTest) { item ->
+
+                    Box(
+                        modifier = Modifier
+                            .padding(12.dp)
+                            .background(Color.Blue),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        Text(
+                            item,
+                            style = MaterialTheme.typography.titleLarge,
+                            color = Color.White
+                        )
+                    }
                 }
             }
+
+            Button(
+                onClick = { viewModel.startAlarm() },
+                modifier = Modifier
+                    .align(Alignment.BottomStart)
+                    .padding(16.dp)
+            ) {
+                Text("start alarm")
+            }
+
+            Button(
+                onClick = { viewModel.cancelAlarm() },
+                modifier = Modifier
+                    .align(Alignment.BottomEnd)
+                    .padding(16.dp)
+            ) {
+                Text("stop alarm")
+            }
         }
-
-
     }
 }
 
