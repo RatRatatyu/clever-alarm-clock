88342ac2f697f11f6fd3aa4fe3b04b22791c5f51
Margarita <kkom.margarita@gmail.com>
2026-06-21 17:30:53 +0500

    refactor: add @AndroidEntryPoint to MainActivity and clean up unused imports

--- app/src/main/java/com/example/cleveralarmclock/MainActivity.kt
+++ app/src/main/java/com/example/cleveralarmclock/MainActivity.kt
@@ -17,7 +17,6 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
@@ -32,11 +31,11 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.core.content.ContextCompat
-import com.example.cleveralarmclock.alarmHandler.AlarmSchedule
 import com.example.cleveralarmclock.presentation.mainScreenFeature.MainScreen
 import com.example.cleveralarmclock.ui.theme.CleverAlarmClockTheme
+import dagger.hilt.android.AndroidEntryPoint
 
-
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
