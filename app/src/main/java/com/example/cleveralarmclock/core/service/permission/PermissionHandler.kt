90d22c1744a3b13f76ca7175bd7c743e1452c8b7
Margarita <kkom.margarita@gmail.com>
2026-07-01 01:30:53 +0500

    feat: implement PermissionHandler with Hilt injection and permission check logic

--- app/src/main/java/com/example/cleveralarmclock/core/service/permission/PermissionHandler.kt
+++ app/src/main/java/com/example/cleveralarmclock/core/service/permission/PermissionHandler.kt
@@ -1,5 +1,20 @@
 package com.example.cleveralarmclock.core.service.permission
 
-class PermissionHandler {
+import android.content.Context
+import android.content.pm.PackageManager
+import androidx.core.content.ContextCompat
+import dagger.hilt.android.qualifiers.ApplicationContext
+import javax.inject.Inject
 
+//Camera Notification
+
+class PermissionHandler @Inject constructor(
+    @ApplicationContext private val context: Context,
+    ){
+
+    fun hasPermission(permission: String): Boolean {
+        return ContextCompat.checkSelfPermission(
+            context, permission
+        ) == PackageManager.PERMISSION_GRANTED
+    }
 }
