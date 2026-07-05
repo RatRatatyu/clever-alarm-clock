2a3e65616d5ebc4a6e121c6b984a174aa4d9d4c8
Margarita <kkom.margarita@gmail.com>
2026-07-05 14:49:50 +0500

    feat: replace alarm control buttons with FloatingActionButton in MainScreen

--- app/src/main/java/com/example/cleveralarmclock/presentation/mainScreenFeature/MainScreen.kt
+++ app/src/main/java/com/example/cleveralarmclock/presentation/mainScreenFeature/MainScreen.kt
@@ -1,12 +1,16 @@
 package com.example.cleveralarmclock.presentation.mainScreenFeature
 
+
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.Button
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -14,21 +18,31 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.example.cleveralarmclock.presentation.mainScreenFeature.data.MainViewModel
 
-
 @Composable
 fun MainScreen(
+    modifier: Modifier = Modifier,
     viewModel: MainViewModel = hiltViewModel(),
-    modifier: Modifier = Modifier
 ) {
     val listForTest = arrayListOf("item1", "item2")
+
+
     Scaffold(
         modifier = modifier
-            .fillMaxSize()
+            .fillMaxSize(),
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = {viewModel.startAlarm()} //for test
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Add,
+                    contentDescription = "Добавить будильник"
+                )
+            }
+        }
     ) { innerPadding ->
         Box(
             modifier = Modifier
@@ -56,33 +70,7 @@ fun MainScreen(
                     }
                 }
             }
-
-            Button(
-                onClick = { viewModel.startAlarm() },
-                modifier = Modifier
-                    .align(Alignment.BottomStart)
-                    .padding(16.dp)
-            ) {
-                Text("start alarm")
-            }
-
-            Button(
-                onClick = { viewModel.cancelAlarm() },
-                modifier = Modifier
-                    .align(Alignment.BottomEnd)
-                    .padding(16.dp)
-            ) {
-                Text("stop alarm")
-            }
         }
     }
 }
 
-@Preview
-@Composable
-private fun MainScreenPrev() {
-    MaterialTheme{
-        MainScreen()
-    }
-
-}
