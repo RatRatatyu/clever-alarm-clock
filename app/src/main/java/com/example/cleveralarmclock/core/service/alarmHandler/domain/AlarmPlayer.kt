224b0d8baccfae0e3394f8936ef583925f8c1be1
Margarita <kkom.margarita@gmail.com>
2026-07-08 01:50:47 +0500

    feat: add AlarmPlayer class with Hilt injection

---
+++ app/src/main/java/com/example/cleveralarmclock/core/service/alarmHandler/domain/AlarmPlayer.kt
@@ -0,0 +1,14 @@
+package com.example.cleveralarmclock.core.service.alarmHandler.domain
+
+import android.content.Context
+import dagger.hilt.android.qualifiers.ApplicationContext
+import javax.inject.Inject
+
+
+class AlarmPlayer @Inject constructor(
+    @ApplicationContext private val context: Context
+){
+
+
+
+}
