f1a07bab5ff723fe1aa279ca673051bd5aae2613
Margarita <kkom.margarita@gmail.com>
2026-07-10 02:14:08 +0500

    feat: add stopAlarmService and update alarm control methods in MainViewModel

--- app/src/main/java/com/example/cleveralarmclock/presentation/mainScreenFeature/data/MainViewModel.kt
+++ app/src/main/java/com/example/cleveralarmclock/presentation/mainScreenFeature/data/MainViewModel.kt
@@ -1,18 +1,28 @@
 package com.example.cleveralarmclock.presentation.mainScreenFeature.data
 
+import android.content.Context
+import android.content.Intent
 import androidx.lifecycle.ViewModel
-import com.example.cleveralarmclock.core.service.alarmHandler.AlarmSchedule
+import com.example.cleveralarmclock.core.service.alarmHandler.domain.AlarmSchedule
+import com.example.cleveralarmclock.core.service.alarmHandler.receiver_service.AlarmService
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 @HiltViewModel
 class MainViewModel @Inject constructor(
-    val alarmSchedule: AlarmSchedule
-): ViewModel(){
-    fun startAlarm(){
+    val alarmSchedule: AlarmSchedule,
+    @dagger.hilt.android.qualifiers.ApplicationContext private val context: Context
+): ViewModel() {
+
+    fun startAlarm() {
         alarmSchedule.schedule()
     }
 
-    fun cancelAlarm(){
+    fun stopAlarm(){
         alarmSchedule.cancel()
+    }
+
+    fun stopAlarmService() {
+        val intent = Intent(context, AlarmService::class.java)
+        context.stopService(intent)
     }
 }
