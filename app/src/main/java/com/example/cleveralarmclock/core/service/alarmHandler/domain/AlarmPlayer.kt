ee9e85ad992b61113072655fbd31b0828279509a
Margarita <kkom.margarita@gmail.com>
2026-07-10 02:13:25 +0500

    feat: implement ExoPlayer logic in AlarmPlayer for alarm playback

--- app/src/main/java/com/example/cleveralarmclock/core/service/alarmHandler/domain/AlarmPlayer.kt
+++ app/src/main/java/com/example/cleveralarmclock/core/service/alarmHandler/domain/AlarmPlayer.kt
@@ -1,14 +1,57 @@
 package com.example.cleveralarmclock.core.service.alarmHandler.domain
 
 import android.content.Context
+import android.util.Log
+import androidx.core.net.toUri
+import androidx.media3.common.AudioAttributes
+import androidx.media3.common.C
+import androidx.media3.common.MediaItem
+import androidx.media3.exoplayer.ExoPlayer
+import com.example.cleveralarmclock.R
 import dagger.hilt.android.qualifiers.ApplicationContext
 import javax.inject.Inject
 
-
 class AlarmPlayer @Inject constructor(
     @ApplicationContext private val context: Context
-){
+) {
+    private var exoPlayer: ExoPlayer? = null
+
+    fun registrationPlayer() {
+        val audioAttributes = AudioAttributes.Builder()
+            .setUsage(C.USAGE_ALARM)
+            .setContentType(C.AUDIO_CONTENT_TYPE_SONIFICATION)
+            .build()
 
 
+        exoPlayer = ExoPlayer.Builder(context)
+            .setAudioAttributes(audioAttributes, true)
+            .build()
+    }
 
+    fun startPlayer() {
+        Log.i("ALARM_DEBUGER", "start music")
+
+        if (exoPlayer == null) {
+            registrationPlayer()
+        }
+
+        val rawUri = "android.resource://${context.packageName}/${R.raw.alarm_classic}".toUri()
+        val mediaItem = MediaItem.fromUri(rawUri)
+
+        exoPlayer?.apply {
+            setMediaItem(mediaItem)
+            repeatMode = ExoPlayer.REPEAT_MODE_ONE
+            prepare()
+            play()
+        }
+    }
+
+    fun stopPlayer() {
+        Log.i("ALARM_DEBUGER", "stop music")
+        exoPlayer?.apply {
+            stop()
+            release()
+        }
+        exoPlayer = null
+    }
 }
