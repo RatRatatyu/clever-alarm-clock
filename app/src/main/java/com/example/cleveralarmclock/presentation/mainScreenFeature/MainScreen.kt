477de8a86fff7227dc94fd18b3604bdae705d5cb
Margarita <kkom.margarita@gmail.com>
2026-07-11 22:19:49 +0500

    refactor: remove unused imports and test list in MainScreen

--- app/src/main/java/com/example/cleveralarmclock/presentation/mainScreenFeature/MainScreen.kt
+++ app/src/main/java/com/example/cleveralarmclock/presentation/mainScreenFeature/MainScreen.kt
@@ -1,26 +1,18 @@
 package com.example.cleveralarmclock.presentation.mainScreenFeature
 
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.Button
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.example.cleveralarmclock.presentation.mainScreenFeature.data.MainViewModel
 
@@ -29,8 +21,6 @@ fun MainScreen(
     modifier: Modifier = Modifier,
     viewModel: MainViewModel = hiltViewModel(),
 ) {
-    val listForTest = arrayListOf("item1", "item2")
-
 
     Scaffold(
         modifier = modifier
