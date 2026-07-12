ef8a70bb9bb4a86fe2bbb7049968acfe08980d4d
Margarita <kkom.margarita@gmail.com>
2026-07-12 13:25:31 +0500

    refactor: clean up imports and comments in SettingsAlarmScreen

--- app/src/main/java/com/example/cleveralarmclock/presentation/manageAlarmFeature/SettingsAlarmScreen.kt
+++ app/src/main/java/com/example/cleveralarmclock/presentation/manageAlarmFeature/SettingsAlarmScreen.kt
@@ -1,33 +1,22 @@
 package com.example.cleveralarmclock.presentation.manageAlarmFeature
 
-import android.content.res.Resources
 import androidx.compose.foundation.background
-import androidx.compose.foundation.gestures.snapping.rememberSnapFlingBehavior
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.RangeSlider
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ModifierLocalBeyondBoundsLayout
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.cleveralarmclock.core.ui.WheelPicker
-import org.intellij.lang.annotations.JdkConstants
 
 @Composable
 fun SettingsAlarm(modifier: Modifier = Modifier) {
@@ -55,10 +44,10 @@ fun SettingsAlarm(modifier: Modifier = Modifier) {
                 ){
                     // HOUR COLUMN
                     Column (
-                        modifier = Modifier // Use capital 'M' Modifier here to avoid inheriting external padding
+                        modifier = Modifier
                             .weight(1f)
                             .fillMaxSize(),
-                        horizontalAlignment = Alignment.CenterHorizontally // <--- ADD THIS
+                        horizontalAlignment = Alignment.CenterHorizontally
                     ){
                         Text("Hours", style = MaterialTheme.typography.titleSmall)
                         WheelPicker()
@@ -69,7 +58,7 @@ fun SettingsAlarm(modifier: Modifier = Modifier) {
                         modifier = Modifier
                             .weight(1f)
                             .fillMaxSize(),
-                        horizontalAlignment = Alignment.CenterHorizontally // <--- ADD THIS
+                        horizontalAlignment = Alignment.CenterHorizontally
                     ){
                         Text("Minutes", style = MaterialTheme.typography.titleSmall)
                         WheelPicker()
