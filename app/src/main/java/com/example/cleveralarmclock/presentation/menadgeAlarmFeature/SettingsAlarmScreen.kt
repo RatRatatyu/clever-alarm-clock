29471ce913497fef483b511234dcfd0b33294e9f
Margarita <kkom.margarita@gmail.com>
2026-07-12 13:24:27 +0500

    feat: implement SettingsAlarm UI and rename package to manageAlarmFeature

--- app/src/main/java/com/example/cleveralarmclock/presentation/menadgeAlarmFeature/SettingsAlarmScreen.kt
+++
@@ -1,9 +0,0 @@
-package com.example.cleveralarmclock.presentation.menadgeAlarmFeature
-
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-
-@Composable
-fun SettingsAlarm(modifier: Modifier = Modifier) {
-    
-}
