998af15e830dfa4b59321d2f7ef1e1acdbf51167
Margarita <kkom.margarita@gmail.com>
2026-07-17 17:48:29 +0500

    feat: implement AlarmRepositoryImpl to handle alarm database operations

---
+++ app/src/main/java/com/example/cleveralarmclock/core/data/repository/AlarmRepositoryImpl.kt
@@ -0,0 +1,31 @@
+package com.example.cleveralarmclock.core.data.repository
+
+import com.example.cleveralarmclock.core.database.dao.AlarmDao
+import com.example.cleveralarmclock.core.database.entity.AlarmEntity
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class AlarmRepositoryImpl @Inject constructor(
+    private val alarmDao: AlarmDao
+) : AlarmRepository {
+
+    override fun getAllAlarms(): Flow<List<AlarmEntity>> {
+        return alarmDao.getAllAlarmClock()
+    }
+
+    override suspend fun insertAlarm(alarm: AlarmEntity) {
+        alarmDao.insert(alarm)
+    }
+
+    override suspend fun deleteAlarm(alarm: AlarmEntity) {
+        alarmDao.delete(alarm)
+    }
+
+    override suspend fun deleteById(alarmId: Int) {
+        alarmDao.deleteById(alarmId)
+    }
+
+    override suspend fun updateAlarm(alarm: AlarmEntity) {
+        alarmDao.update(alarm)
+    }
+}
