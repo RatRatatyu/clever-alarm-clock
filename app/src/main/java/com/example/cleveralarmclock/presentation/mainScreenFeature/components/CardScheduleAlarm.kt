1fa9db17df82127f20164e6bde66fb4f71e3e1c0
Margarita <kkom.margarita@gmail.com>
2026-07-20 14:09:16 +0500

    feat: implement CardScheduledAlarm component for displaying scheduled alarms

---
+++ app/src/main/java/com/example/cleveralarmclock/presentation/mainScreenFeature/components/CardScheduleAlarm.kt
@@ -0,0 +1,100 @@
+package com.example.cleveralarmclock.presentation.mainScreenFeature.components
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Switch
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun CardScheduledAlarm(
+    modifier: Modifier = Modifier,
+    hour: String,
+    minute: String,
+    isActive: Boolean,
+    onChanged: () -> Unit
+) {
+    Card(
+        modifier = modifier
+            .fillMaxWidth()
+            .height(100.dp)
+            .padding(10.dp),
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.surfaceVariant,
+        ),
+    ) {
+        Row (
+            Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically
+        ){
+
+            Column(
+                Modifier.weight(2f)
+            ) {
+
+                Row(
+                    Modifier
+                        .fillMaxWidth()
+                        .weight(2f)
+                        .padding(horizontal = 10.dp),
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Text(
+                        "$hour:$minute",
+                        style = MaterialTheme.typography.headlineLarge
+                    )
+                }
+
+
+                Row(
+                    Modifier
+                        .fillMaxWidth()
+                        .weight(1f)
+                        .padding(horizontal = 10.dp),
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Text("Mon-Fri") //will change to real data later
+                }
+            }
+
+            Column(
+                Modifier
+                    .weight(1f)
+                    .padding(10.dp),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ){
+                Switch(checked = isActive, onCheckedChange = { _ -> onChanged() })
+            }
+        }
+    }
+}
+
+
+@Preview(showSystemUi = true)
+@Composable
+fun CardScheduledAlarmPrev(){
+    MaterialTheme{
+        Column (
+            Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ){
+            CardScheduledAlarm(hour = "12", minute = "00", isActive = false, onChanged = {})
+        }
+    }
+}
