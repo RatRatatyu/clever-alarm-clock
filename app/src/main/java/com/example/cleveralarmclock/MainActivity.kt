82f5e485ae4e34f80164790a7b30a19c344c9fe3
Margarita <kkom.margarita@gmail.com>
2026-07-21 13:56:16 +0500

    refactor: remove ScreenForAlarm test composable and unused imports in MainActivity

--- app/src/main/java/com/example/cleveralarmclock/MainActivity.kt
+++ app/src/main/java/com/example/cleveralarmclock/MainActivity.kt
@@ -8,33 +8,10 @@ import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
-import androidx.compose.material3.Button
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
-import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.hilt.navigation.compose.hiltViewModel
 import com.example.cleveralarmclock.core.service.permission.PermissionHandler
 import com.example.cleveralarmclock.core.service.permission.PermissionTypes
-import com.example.cleveralarmclock.presentation.mainScreenFeature.MainScreen
-import com.example.cleveralarmclock.presentation.mainScreenFeature.MainViewModel
 import com.example.cleveralarmclock.presentation.navigation.AppNavHost
-import com.example.cleveralarmclock.ui.theme.CleverAlarmClockTheme
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
 
@@ -45,8 +22,6 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        val isFormAlarm = intent.getBooleanExtra("ALARM_TRIGGERED", false)
 
         enableEdgeToEdge()
         setContent {
@@ -76,44 +51,3 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
-
-
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-fun ScreenForAlarm( //for testing
-    modifier: Modifier = Modifier,
-    viewModel: MainViewModel = hiltViewModel(),
-) {
-    Scaffold(
-        Modifier.fillMaxSize(),
-        {
-            TopAppBar(
-                title = { Text("hi") }
-            )
-        }
-
-    ) { innerPadding ->
-
-        Column (
-            modifier
-                .fillMaxSize()
-                .padding(innerPadding),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
-
-        ){
-            Box(
-                modifier
-                    .height(100.dp)
-                    .width(100.dp)
-                    .background(color = Color.Blue),
-                contentAlignment = Alignment.Center
-            ){
-                Button(onClick = { viewModel.stopAlarmService() }){Text("stop sound")}
-            }
-        }
-
-    }
-}
-
-
