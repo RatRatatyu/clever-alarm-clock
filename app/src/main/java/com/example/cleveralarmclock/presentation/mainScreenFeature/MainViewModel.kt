7ffdb71c891330079e206779de9bba9f0c194ea6
Margarita <kkom.margarita@gmail.com>
2026-07-21 13:56:33 +0500

    refactor: update MainViewModel to use GetAlarmsUseCase and ToggleAlarmUseCase and remove direct alarm scheduling logic

--- app/src/main/java/com/example/cleveralarmclock/presentation/mainScreenFeature/MainViewModel.kt
+++ app/src/main/java/com/example/cleveralarmclock/presentation/mainScreenFeature/MainViewModel.kt
@@ -1,48 +1,27 @@
 package com.example.cleveralarmclock.presentation.mainScreenFeature
 
-import android.content.Context
-import android.content.Intent
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.example.cleveralarmclock.core.data.repository.AlarmRepository
 import com.example.cleveralarmclock.core.database.entity.AlarmEntity
-import com.example.cleveralarmclock.core.service.alarmHandler.domain.AlarmSchedule
-import com.example.cleveralarmclock.core.service.alarmHandler.receiver_service.AlarmService
+import com.example.cleveralarmclock.core.domain.usecase.GetAlarmsUseCase
+import com.example.cleveralarmclock.core.domain.usecase.ToggleAlarmUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
-import dagger.hilt.android.qualifiers.ApplicationContext
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class MainViewModel @Inject constructor(
-    val alarmSchedule: AlarmSchedule,
-    val alarmRepository: AlarmRepository,
-    @ApplicationContext private val context: Context
+    getAlarmsUseCase: GetAlarmsUseCase,
+    private val toggleAlarmUseCase: ToggleAlarmUseCase
 ): ViewModel() {
 
-    val scheduleFlow: Flow<List<AlarmEntity>> = alarmRepository.getAllAlarms()
+    val scheduleFlow: Flow<List<AlarmEntity>> = getAlarmsUseCase()
 
     fun toggleAlarmStatus(alarm: AlarmEntity){
         viewModelScope.launch {
-            alarmRepository.updateAlarm(
-                alarm.copy(
-                    isActivate = !alarm.isActivate
-                )
-            )
+            toggleAlarmUseCase(alarm)
         }
     }
 
-    fun startAlarm(hour: Int, minute: Int) {
-        alarmSchedule.schedule(hour, minute)
-    }
-
-    fun stopAlarm(){
-        alarmSchedule.cancel()
-    }
-
-    fun stopAlarmService() {
-        val intent = Intent(context, AlarmService::class.java)
-        context.stopService(intent)
-    }
 }
