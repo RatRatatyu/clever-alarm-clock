44a134890310b48558212ddb95020953163a2c86
Margarita <kkom.margarita@gmail.com>
2026-07-22 21:48:03 +0500

    feat: add CameraTaskFeature composable for wake up tasks

---
+++ app/src/main/java/com/example/cleveralarmclock/presentation/alarmAlertFeature/presentation/CameraTaskFeature.kt
@@ -0,0 +1,36 @@
+package com.example.cleveralarmclock.presentation.alarmAlertFeature.presentation
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.hilt.navigation.compose.hiltViewModel
+
+
+
+//this composable will be used to display a wake up task
+//but now we used it for test
+
+@Composable
+fun CameraTaskFeature(
+    modifier: Modifier = Modifier,
+    viewModel: CameraTaskViewModel = hiltViewModel()
+){
+    Scaffold (
+        modifier
+            .fillMaxSize()
+    ){ innerPadding ->
+
+        Column(
+            Modifier
+                .fillMaxSize()
+                .padding(innerPadding)
+        ){
+            Text("Wake up ${viewModel.alarmId}")
+        }
+
+    }
+}
