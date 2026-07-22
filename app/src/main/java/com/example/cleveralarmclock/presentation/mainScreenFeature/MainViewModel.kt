af3a8ff69e2cf7bd41b6a3e3d432062759166cd0
Margarita <kkom.margarita@gmail.com>
2026-07-22 21:50:11 +0500

    feat: update MainViewModel to provide next alarm time state and convert scheduleFlow to StateFlow

--- app/src/main/java/com/example/cleveralarmclock/presentation/mainScreenFeature/MainViewModel.kt
+++ app/src/main/java/com/example/cleveralarmclock/presentation/mainScreenFeature/MainViewModel.kt
@@ -4,24 +4,56 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.cleveralarmclock.core.database.entity.AlarmEntity
 import com.example.cleveralarmclock.core.domain.usecase.GetAlarmsUseCase
+import com.example.cleveralarmclock.core.domain.usecase.GetNextAlarmTimeUseCase
 import com.example.cleveralarmclock.core.domain.usecase.ToggleAlarmUseCase
+import com.example.cleveralarmclock.presentation.mainScreenFeature.util.TimeRemainingFormatter
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class MainViewModel @Inject constructor(
     getAlarmsUseCase: GetAlarmsUseCase,
-    private val toggleAlarmUseCase: ToggleAlarmUseCase
+    private val toggleAlarmUseCase: ToggleAlarmUseCase,
+    private val getNextAlarmTimeUseCase: GetNextAlarmTimeUseCase,
+    private val timeRemainingFormatter: TimeRemainingFormatter
 ): ViewModel() {
 
-    val scheduleFlow: Flow<List<AlarmEntity>> = getAlarmsUseCase()
+
+    val scheduleFlow: StateFlow<List<AlarmEntity>> = getAlarmsUseCase()
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5000),
+            initialValue = emptyList()
+        )
+    val nextAlarmTime: StateFlow<String?> = getNextAlarmTimeUseCase()
+        .map { timeRemaining ->
+            if (timeRemaining == null) {
+                null
+            } else {
+                timeRemainingFormatter.format(
+                    hours = timeRemaining.hours,
+                    minutes = timeRemaining.minutes
+                )
+            }
+        }
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5000),
+            initialValue = null
+        )
 
     fun toggleAlarmStatus(alarm: AlarmEntity){
         viewModelScope.launch {
             toggleAlarmUseCase(alarm)
         }
     }
+
+
 
 }
