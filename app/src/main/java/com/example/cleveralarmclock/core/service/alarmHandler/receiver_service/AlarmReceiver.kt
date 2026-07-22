6590706b01fd45765ff042c3dabbfd39518a4b3f
Margarita <kkom.margarita@gmail.com>
2026-07-22 21:48:48 +0500

    feat: update AlarmReceiver to extract and pass alarm ID to AlarmService

--- app/src/main/java/com/example/cleveralarmclock/core/service/alarmHandler/receiver_service/AlarmReceiver.kt
+++ app/src/main/java/com/example/cleveralarmclock/core/service/alarmHandler/receiver_service/AlarmReceiver.kt
@@ -16,13 +16,18 @@ class AlarmReceiver : BroadcastReceiver() {
 
     override fun onReceive(context: Context?, intent: Intent?) {
         val ctx = context ?: return
+        val alarmId = intent?.getIntExtra("ALARM_ID", 0)
+        Log.i("AlARM_DEBUG", "$alarmId")
+
 
         if (intent?.action == "android.intent.action.BOOT_COMPLETED"){
             // TODO
         }else{
 
             Log.i("ALARM_DEBUG", "Время пришло! Показываем уведомление звонка.")
-            val foregroundServiceIntent = Intent(ctx, AlarmService::class.java)
+            val foregroundServiceIntent = Intent(ctx, AlarmService::class.java).apply {
+                putExtra("ALARM_ID", alarmId)
+            }
             ctx.startForegroundService(foregroundServiceIntent)
 
         }
