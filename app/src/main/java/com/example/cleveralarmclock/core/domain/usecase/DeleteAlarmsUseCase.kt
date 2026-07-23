67ef85935a837e4cdfc0f3ab8f550e8eada13c16
Margarita <kkom.margarita@gmail.com>
2026-07-24 00:07:54 +0500

    feat: implement DeleteAlarmsUseCase

---
+++ app/src/main/java/com/example/cleveralarmclock/core/domain/usecase/DeleteAlarmsUseCase.kt
@@ -0,0 +1,18 @@
+package com.example.cleveralarmclock.core.domain.usecase
+
+import com.example.cleveralarmclock.core.data.database.entity.AlarmEntity
+import com.example.cleveralarmclock.core.domain.alarm.AlarmSchedule
+import com.example.cleveralarmclock.core.domain.repository.AlarmRepository
+import javax.inject.Inject
+
+class DeleteAlarmsUseCase @Inject constructor(
+    private val alarmRepository: AlarmRepository,
+    private val alarmSchedule: AlarmSchedule
+) {
+    suspend operator fun invoke(alarms: List<Int>){
+        alarms.forEach { id ->
+            alarmSchedule.cancel(id)
+        }
+        alarmRepository.deleteAlarmsByIds(alarms)
+    }
+}
