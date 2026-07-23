d88fba6f73a866602882b312afe3870cdcaab26c
Margarita <kkom.margarita@gmail.com>
2026-07-23 23:05:54 +0500

    refactor: inject AlarmPlayer into CameraTaskViewModel and add stopMusic method

--- app/src/main/java/com/example/cleveralarmclock/presentation/alarmAlertFeature/presentation/CameraTaskViewModel.kt
+++ app/src/main/java/com/example/cleveralarmclock/presentation/alarmAlertFeature/presentation/CameraTaskViewModel.kt
@@ -2,14 +2,20 @@ package com.example.cleveralarmclock.presentation.alarmAlertFeature.presentation
 
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import com.example.cleveralarmclock.core.domain.alarm.AlarmPlayer
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
 class CameraTaskViewModel @Inject constructor(
     savedStateHandle: SavedStateHandle,
+    private val alarmPlayer: AlarmPlayer
 ): ViewModel(){
 
     val alarmId: Int = savedStateHandle.get<Int>("ALARM_ID") ?: -1
+
+    fun stopMusic(){
+        alarmPlayer.stopPlayer()
+    }
 
 }
