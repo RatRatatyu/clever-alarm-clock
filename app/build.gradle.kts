18e4cd3dd7a298b8c30cb6e5edc77a139161fca4
Margarita <kkom.margarita@gmail.com>
2026-07-24 23:31:14 +0500

    build: add androidx.compose.foundation.layout dependency to app/build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,6 +47,7 @@ android {
 dependencies {
     implementation(libs.androidx.room.runtime)
     implementation(libs.androidx.room.ktx)
+    implementation(libs.androidx.compose.foundation.layout)
     ksp(libs.androidx.room.compiler)
 
     implementation(libs.androidx.media3.session)
