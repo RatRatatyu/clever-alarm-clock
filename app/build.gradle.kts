28ae709b16c688657f7d781d260b850544c5f9c2
Margarita <kkom.margarita@gmail.com>
2026-07-25 17:42:08 +0500

    build: simplify compileSdk syntax in app/build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,9 +7,7 @@ plugins {
 }
 android {
     namespace = "com.example.cleveralarmclock"
-    compileSdk {
-        version = release(36)
-    }
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.example.cleveralarmclock"
