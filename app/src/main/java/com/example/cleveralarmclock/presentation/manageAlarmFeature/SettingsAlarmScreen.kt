f12f6cd7e66b9fc165d6b13e611f5dc2349c3557
Margarita <kkom.margarita@gmail.com>
2026-07-27 01:49:45 +0500

    refactor: remove unused Locale import in SettingsAlarmScreen.kt

--- app/src/main/java/com/example/cleveralarmclock/presentation/manageAlarmFeature/SettingsAlarmScreen.kt
+++ app/src/main/java/com/example/cleveralarmclock/presentation/manageAlarmFeature/SettingsAlarmScreen.kt
@@ -1,6 +1,5 @@
 package com.example.cleveralarmclock.presentation.manageAlarmFeature
 
-import java.util.Locale
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
