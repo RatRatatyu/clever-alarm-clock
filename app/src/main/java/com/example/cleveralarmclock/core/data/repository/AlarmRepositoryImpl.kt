b677037ca2bf859ab8bea203afcb9ac937061ef7
Margarita <kkom.margarita@gmail.com>
2026-07-27 01:48:44 +0500

    feat: update AlarmRepositoryImpl to use AlarmModel and AlarmMapper

--- app/src/main/java/com/example/cleveralarmclock/core/data/repository/AlarmRepositoryImpl.kt
+++ app/src/main/java/com/example/cleveralarmclock/core/data/repository/AlarmRepositoryImpl.kt
@@ -1,36 +1,49 @@
 package com.example.cleveralarmclock.core.data.repository
 
 import com.example.cleveralarmclock.core.data.database.dao.AlarmDao
-import com.example.cleveralarmclock.core.data.database.entity.AlarmEntity
+import com.example.cleveralarmclock.core.data.mapper.AlarmMapper
+import com.example.cleveralarmclock.core.domain.module.AlarmModel
 import com.example.cleveralarmclock.core.domain.repository.AlarmRepository
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
 class AlarmRepositoryImpl @Inject constructor(
-    private val alarmDao: AlarmDao
+    private val alarmDao: AlarmDao,
+    private val alarmMapper: AlarmMapper
 ) : AlarmRepository {
 
-    override fun getAllAlarms(): Flow<List<AlarmEntity>> {
-        return alarmDao.getAllAlarmClock()
+    override fun getAllAlarms(): Flow<List<AlarmModel>> {
+        return alarmDao.getAllAlarmClock().map { entityList->
+            with(alarmMapper) {entityList.toDomainList()}
+        }
     }
 
-    override suspend fun insertAlarm(alarm: AlarmEntity): Long {
-        return alarmDao.insert(alarm)
+    override suspend fun insertAlarm(alarm: AlarmModel): Long {
+        val entity = with(alarmMapper) { alarm.toEntity() }
+
+        return alarmDao.insert(entity)
     }
 
     override suspend fun deleteAlarmsByIds(ids: List<Int>) {
         alarmDao.deleteAlarmsByIds(ids)
     }
 
-    override fun getActiveAlarms(): Flow<List<AlarmEntity>> {
-        return alarmDao.getActiveAlarms()
+    override fun getActiveAlarms(): Flow<List<AlarmModel>> {
+        return alarmDao.getActiveAlarms().map { entityList ->
+            with(alarmMapper) {entityList.toDomainList()}
+        }
     }
 
-    override suspend fun updateAlarm(alarm: AlarmEntity) {
-        alarmDao.update(alarm)
+    override suspend fun updateAlarm(alarm: AlarmModel) {
+        val entity = with(alarmMapper){alarm.toEntity()}
+
+        alarmDao.update(entity)
     }
 
-    override suspend fun getAlarmById(id: Int): AlarmEntity? {
-        return alarmDao.getAlarmById(id)
+    override suspend fun getAlarmById(id: Int): AlarmModel? {
+        val entity = alarmDao.getAlarmById(id) ?: return null
+
+        return with(alarmMapper) { entity.toDomain() }
     }
 }
