0e1c56bd727148695c3179de4d6a09d2fa1f2daa
Margarita <kkom.margarita@gmail.com>
2026-07-27 01:46:55 +0500

    feat: update AddAlarmUseCase to use AlarmModel

--- app/src/main/java/com/example/cleveralarmclock/core/domain/usecase/AddAlarmUseCase.kt
+++ app/src/main/java/com/example/cleveralarmclock/core/domain/usecase/AddAlarmUseCase.kt
@@ -1,15 +1,18 @@
 package com.example.cleveralarmclock.core.domain.usecase
 
 import com.example.cleveralarmclock.core.domain.repository.AlarmRepository
-import com.example.cleveralarmclock.core.data.database.entity.AlarmEntity
+import com.example.cleveralarmclock.core.data.mapper.AlarmMapper
 import com.example.cleveralarmclock.core.domain.alarm.AlarmSchedule
+import com.example.cleveralarmclock.core.domain.module.AlarmModel
 import javax.inject.Inject
 
 class AddAlarmUseCase @Inject constructor(
     private val alarmRepository: AlarmRepository,
     private val alarmSchedule: AlarmSchedule,
+    private val alarmMapper: AlarmMapper
 ){
-    suspend operator fun invoke(alarm: AlarmEntity){
+    suspend operator fun invoke(alarm: AlarmModel){
+
         val alarmId = alarmRepository.insertAlarm(alarm)
 
         alarmSchedule.schedule(
