31fd4d04ce8145f973664f950df748bc2c0f986f
Margarita <kkom.margarita@gmail.com>
2026-07-27 01:48:56 +0500

    refactor: change GetAlarmByIdUseCase return type to AlarmModel

--- app/src/main/java/com/example/cleveralarmclock/core/domain/usecase/GetAlarmByIdUseCase.kt
+++ app/src/main/java/com/example/cleveralarmclock/core/domain/usecase/GetAlarmByIdUseCase.kt
@@ -1,13 +1,13 @@
 package com.example.cleveralarmclock.core.domain.usecase
 
-import com.example.cleveralarmclock.core.data.database.entity.AlarmEntity
+import com.example.cleveralarmclock.core.domain.module.AlarmModel
 import com.example.cleveralarmclock.core.domain.repository.AlarmRepository
 import javax.inject.Inject
 
 class GetAlarmByIdUseCase @Inject constructor(
     private val repository: AlarmRepository
 ) {
-    suspend operator fun invoke(alarmId: Int): AlarmEntity?{
+    suspend operator fun invoke(alarmId: Int): AlarmModel?{
         return repository.getAlarmById(alarmId)
 
     }
