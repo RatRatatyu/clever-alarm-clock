5ea52c53206e085d19ba737c4b104fb899be1113
Margarita <kkom.margarita@gmail.com>
2026-07-30 02:13:43 +0500

    refactor: remove unused AlarmEntity import in DeleteAlarmsUseCase

--- app/src/main/java/com/example/cleveralarmclock/core/domain/usecase/DeleteAlarmsUseCase.kt
+++ app/src/main/java/com/example/cleveralarmclock/core/domain/usecase/DeleteAlarmsUseCase.kt
@@ -1,6 +1,5 @@
 package com.example.cleveralarmclock.core.domain.usecase
 
-import com.example.cleveralarmclock.core.data.database.entity.AlarmEntity
 import com.example.cleveralarmclock.core.domain.alarm.AlarmSchedule
 import com.example.cleveralarmclock.core.domain.repository.AlarmRepository
 import javax.inject.Inject
