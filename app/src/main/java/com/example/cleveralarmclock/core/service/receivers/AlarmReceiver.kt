e679c62b64eae400444e13c0241b8942ddc2508f
Margarita <kkom.margarita@gmail.com>
2026-07-29 14:43:58 +0500

    refactor: simplify AlarmReceiver by removing logging and BOOT_COMPLETED check

--- app/src/main/java/com/example/cleveralarmclock/core/service/receivers/AlarmReceiver.kt
+++ app/src/main/java/com/example/cleveralarmclock/core/service/receivers/AlarmReceiver.kt
@@ -3,7 +3,6 @@ package com.example.cleveralarmclock.core.service.receivers
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
-import android.util.Log
 import com.example.cleveralarmclock.core.service.services.AlarmService
 
 class AlarmReceiver : BroadcastReceiver() {
@@ -12,20 +11,11 @@ class AlarmReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context?, intent: Intent?) {
         val ctx = context ?: return
         val alarmId = intent?.getIntExtra("ALARM_ID", 0)
-        Log.i("AlARM_DEBUG", "$alarmId")
 
-
-        if (intent?.action == "android.intent.action.BOOT_COMPLETED"){
-            // TODO
-        }else{
-
-            Log.i("ALARM_DEBUG", "Время пришло! Показываем уведомление звонка.")
-            val foregroundServiceIntent = Intent(ctx, AlarmService::class.java).apply {
-                putExtra("ALARM_ID", alarmId)
-            }
-            ctx.startForegroundService(foregroundServiceIntent)
-
+        val foregroundServiceIntent = Intent(ctx, AlarmService::class.java).apply {
+            putExtra("ALARM_ID", alarmId)
         }
+        ctx.startForegroundService(foregroundServiceIntent)
     }
 
 }
