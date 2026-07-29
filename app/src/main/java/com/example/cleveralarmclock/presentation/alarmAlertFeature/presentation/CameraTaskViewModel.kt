62bae5b6b870e8becfc522d43f5d6d23342c4bb9
Margarita <kkom.margarita@gmail.com>
2026-07-30 02:13:25 +0500

    refactor: replace AlarmPlayer with StopAlarmUseCase in CameraTaskViewModel

--- app/src/main/java/com/example/cleveralarmclock/presentation/alarmAlertFeature/presentation/CameraTaskViewModel.kt
+++ app/src/main/java/com/example/cleveralarmclock/presentation/alarmAlertFeature/presentation/CameraTaskViewModel.kt
@@ -2,20 +2,20 @@ package com.example.cleveralarmclock.presentation.alarmAlertFeature.presentation
 
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
-import com.example.cleveralarmclock.core.domain.alarm.AlarmPlayer
+import com.example.cleveralarmclock.core.domain.usecase.StopAlarmUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
 class CameraTaskViewModel @Inject constructor(
     savedStateHandle: SavedStateHandle,
-    private val alarmPlayer: AlarmPlayer
+    private val stopAlarmUseCase: StopAlarmUseCase
 ): ViewModel(){
 
     val alarmId: Int = savedStateHandle.get<Int>("ALARM_ID") ?: -1
 
     fun stopMusic(){
-        alarmPlayer.stopPlayer()
+        stopAlarmUseCase()
     }
 
 }
