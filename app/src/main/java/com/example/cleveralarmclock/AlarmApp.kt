383c2dc64227039de79263b56fb0d276aca936df
Margarita <kkom.margarita@gmail.com>
2026-07-29 14:43:46 +0500

    build: implement Configuration.Provider in AlarmApp for HiltWorkerFactory integration

--- app/src/main/java/com/example/cleveralarmclock/AlarmApp.kt
+++ app/src/main/java/com/example/cleveralarmclock/AlarmApp.kt
@@ -1,9 +1,20 @@
 package com.example.cleveralarmclock
 
 import android.app.Application
+import androidx.hilt.work.HiltWorkerFactory
+import androidx.work.Configuration
 import dagger.hilt.android.HiltAndroidApp
+import javax.inject.Inject
+
 
 @HiltAndroidApp
-class AlarmApp : Application(){
+class AlarmApp : Application(), Configuration.Provider {
 
+    @Inject
+    lateinit var workerFactory: HiltWorkerFactory
+
+    override val workManagerConfiguration: Configuration
+        get() = Configuration.Builder()
+            .setWorkerFactory(workerFactory)
+            .build()
 }
