849edb5343af60ad20deabfcdb14c6f76ab0316b
Margarita <kkom.margarita@gmail.com>
2026-07-29 14:45:32 +0500

    feat: add RescheduleAlarmsWorker for rescheduling active alarms

---
+++ app/src/main/java/com/example/cleveralarmclock/core/service/worker/RescheduleAlarmsWorker.kt
@@ -0,0 +1,42 @@
+package com.example.cleveralarmclock.core.service.worker
+
+import android.content.Context
+import android.util.Log
+import androidx.hilt.work.HiltWorker
+import androidx.work.CoroutineWorker
+import androidx.work.WorkerParameters
+import com.example.cleveralarmclock.core.domain.alarm.AlarmSchedule
+import com.example.cleveralarmclock.core.domain.repository.AlarmRepository
+import dagger.assisted.Assisted
+import dagger.assisted.AssistedInject
+import kotlinx.coroutines.flow.first
+
+@HiltWorker
+class RescheduleAlarmsWorker @AssistedInject constructor(
+    @Assisted context: Context,
+    @Assisted workerParameters: WorkerParameters,
+    private val alarmRepository: AlarmRepository,
+    private val alarmSchedule: AlarmSchedule
+) : CoroutineWorker(context, workerParameters) {
+
+    override suspend fun doWork(): Result {
+        Log.i("ALARM_DEBUGING", "Alarms rescheduled started!")
+
+        return try {
+            val activeAlarms = alarmRepository.getActiveAlarms().first()
+
+            activeAlarms.forEach { alarm ->
+                alarmSchedule.schedule(
+                    id = alarm.id,
+                    hour = alarm.hours,
+                    minute = alarm.minutes
+                )
+            }
+            Log.i("ALARM_DEBUGING", "Successfully rescheduled ${activeAlarms.size} alarms.")
+            Result.success()
+        } catch (e: Exception) {
+            Log.e("ALARM_DEBUGING", "Error rescheduling alarms", e)
+            Result.retry()
+        }
+    }
+}
