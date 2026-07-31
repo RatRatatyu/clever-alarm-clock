2e45ac87f8f1db63a94879cc16c4ca5b6d40ad1a
Margarita <kkom.margarita@gmail.com>
2026-07-31 23:24:10 +0500

    feat: update StopAlarmUseCase to reschedule repeating alarms or toggle non-repeating ones

--- app/src/main/java/com/example/cleveralarmclock/core/domain/usecase/StopAlarmUseCase.kt
+++ app/src/main/java/com/example/cleveralarmclock/core/domain/usecase/StopAlarmUseCase.kt
@@ -2,20 +2,38 @@ package com.example.cleveralarmclock.core.domain.usecase
 
 import android.content.Context
 import android.content.Intent
+import android.util.Log
 import com.example.cleveralarmclock.core.domain.alarm.AlarmPlayer
+import com.example.cleveralarmclock.core.domain.alarm.AlarmSchedule
+import com.example.cleveralarmclock.core.domain.module.Alarm
 import com.example.cleveralarmclock.core.service.services.AlarmService
 import dagger.hilt.android.qualifiers.ApplicationContext
 import javax.inject.Inject
 
 class StopAlarmUseCase @Inject constructor(
     @param:ApplicationContext private val context: Context,
-    private val alarmPlayer: AlarmPlayer
+    private val alarmPlayer: AlarmPlayer,
+    private val toggleAlarmUseCase: ToggleAlarmUseCase,
+    private val alarmSchedule: AlarmSchedule
 ) {
 
-    operator fun invoke(){
+    suspend operator fun invoke(alarm: Alarm){
         alarmPlayer.stopPlayer()
 
         val intent = Intent(context, AlarmService::class.java)
         context.stopService(intent)
+
+        if(alarm.isRepeated){
+            alarmSchedule.schedule(
+                alarm.hours,
+                alarm.minutes,
+                alarm.repeatDays,
+                alarm.id)
+
+        }else{
+            toggleAlarmUseCase(alarm)
+        }
+        Log.i("ALARM_DEBUDING", "repited alarm set ")
+
     }
 }
