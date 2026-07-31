9deca9fc618a9d616920358f1cae6b12e68b89d5
Margarita <kkom.margarita@gmail.com>
2026-07-31 23:25:02 +0500

    refactor: use AlarmTimeCalculator in AlarmScheduleImpl to support repeat days

--- app/src/main/java/com/example/cleveralarmclock/core/service/alarm/AlarmSchedulerImpl.kt
+++ app/src/main/java/com/example/cleveralarmclock/core/service/alarm/AlarmSchedulerImpl.kt
@@ -7,19 +7,21 @@ import android.content.Context
 import android.content.Intent
 import android.util.Log
 import com.example.cleveralarmclock.core.domain.alarm.AlarmSchedule
+import com.example.cleveralarmclock.core.domain.util.AlarmTimeCalculator
 import com.example.cleveralarmclock.core.service.receivers.AlarmReceiver
 import dagger.hilt.android.qualifiers.ApplicationContext
+import java.time.DayOfWeek
 import java.time.LocalDateTime
 import java.time.ZoneId
 import javax.inject.Inject
 
 class AlarmScheduleImpl @Inject constructor(
-    @param:ApplicationContext private val context: Context,
+    @param:ApplicationContext private val context: Context, 
 ): AlarmSchedule{
     private val alarmManager = context.getSystemService(Context.ALARM_SERVICE) as? AlarmManager
 
     @SuppressLint("MissingPermission")
-    override fun schedule(hour: Int, minute: Int, id: Int){
+    override fun schedule(hour: Int, minute: Int, repeatDays: Set<DayOfWeek>, id: Int){
 
         val intent = Intent(context, AlarmReceiver::class.java).apply {
             putExtra("ALARM_ID", id)
@@ -30,16 +32,11 @@ class AlarmScheduleImpl @Inject constructor(
             intent,
             PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
         )
-        var localTime = LocalDateTime.now()
-            .withHour(hour)
-            .withMinute(minute)
-            .withSecond(0)
-            .withNano(0)
-
-        if (localTime.isBefore(LocalDateTime.now())) {
-            localTime = localTime.plusDays(1)
-        }
-
+        val localTime = AlarmTimeCalculator.calculateNextTriggerTime(
+            hour,
+            minute,
+            repeatDays
+        )
         val triggerTime = localTime
             .atZone(ZoneId.systemDefault())
             .toInstant()
