519f845567c60b20e7beb87132986b017359a0ac
Margarita <kkom.margarita@gmail.com>
2026-07-31 23:25:19 +0500

    refactor: update AlarmMapper to include taskId, repeatDays, and isRepeated fields

--- app/src/main/java/com/example/cleveralarmclock/core/data/mapper/AlarmMapper.kt
+++ app/src/main/java/com/example/cleveralarmclock/core/data/mapper/AlarmMapper.kt
@@ -1,19 +1,24 @@
 package com.example.cleveralarmclock.core.data.mapper
 
 import com.example.cleveralarmclock.core.data.database.entity.AlarmEntity
-
 import com.example.cleveralarmclock.core.domain.module.Alarm
+import java.time.DayOfWeek
 import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
 class AlarmMapper @Inject constructor(){
     fun AlarmEntity.toDomain(): Alarm {
+        val repeatDays = if (this.repeatDays.isEmpty()) emptySet() else
+        this.repeatDays.split(",").map { DayOfWeek.of(it.toInt()) }.toSet()
+
         return Alarm(
             id = this.id,
             hours = this.hours,
             minutes = this.minutes,
-            colorHex = colorHex,
+            taskId = this.taskId,
+            repeatDays = repeatDays,
+            isRepeated = this.isRepeated,
             isActivate = this.isActivate
         )
     }
@@ -23,7 +28,9 @@ class AlarmMapper @Inject constructor(){
             id = this.id,
             hours = this.hours,
             minutes = this.minutes,
-            colorHex = colorHex,
+            taskId = this.taskId,
+            repeatDays = this.repeatDays.joinToString(",") { it.value.toString() },
+            isRepeated = this.isRepeated,
             isActivate = this.isActivate
         )
     }
