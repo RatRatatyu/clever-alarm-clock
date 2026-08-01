1e92afaff4a0d1683891cd8c6f8e8e16d740382e
Margarita <kkom.margarita@gmail.com>
2026-08-01 18:01:37 +0500

    feat: add repeat days selection logic to SettingsAlarmViewModel

--- app/src/main/java/com/example/cleveralarmclock/presentation/manageAlarmFeature/SettingsAlarmViewModel.kt
+++ app/src/main/java/com/example/cleveralarmclock/presentation/manageAlarmFeature/SettingsAlarmViewModel.kt
@@ -6,13 +6,14 @@ import androidx.lifecycle.viewModelScope
 import com.example.cleveralarmclock.core.domain.module.Alarm
 import com.example.cleveralarmclock.core.domain.usecase.AddAlarmUseCase
 import com.example.cleveralarmclock.core.domain.usecase.GetAlarmByIdUseCase
-import com.example.cleveralarmclock.presentation.manageAlarmFeature.util.DataTimeFormatter
+import com.example.cleveralarmclock.core.domain.util.DataTimeFormatter
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
+import java.time.DayOfWeek
 import java.time.LocalTime
 import javax.inject.Inject
 
@@ -20,8 +21,10 @@ import javax.inject.Inject
 data class SettingAlarmState(
     val selectedHours: Int = 0,
     val selectedMinutes: Int = 0,
-    val is24Hours: Boolean = true,
     val selectedAmPm: String = "AM",
+    val selectedDayOfWeek: Set<DayOfWeek> = setOf(),
+    val isAllDaysSelected: Boolean = false,
+    val is24Hours: Boolean = true,
     val isLoading: Boolean = false,
 )
 
@@ -57,12 +60,15 @@ class SettingsAlarmViewModel @Inject constructor(
         viewModelScope.launch {
             val alarm = getAlarmByIdUseCase(alarmId)
             val formater = dataTimeFormatter.convert24To12Hour(alarm?.hours ?: -1)
+            val days = alarm?.repeatDays ?: emptySet()
 
             _uiState.update {
                 it.copy(
                     selectedHours = formater.hour,
                     selectedMinutes = alarm?.minutes ?: LocalTime.now().minute,
                     selectedAmPm = formater.amPm,
+                    selectedDayOfWeek = alarm?.repeatDays ?: setOf(),
+                    isAllDaysSelected = days.size == 7,
                     is24Hours = formater.is24Format,
                     isLoading = false
                 )
@@ -108,6 +114,40 @@ class SettingsAlarmViewModel @Inject constructor(
         }
     }
 
+    fun selectAllDayOfWeek(){
+        _uiState.update { state ->
+            val newAllSelectedValue = !state.isAllDaysSelected
+
+            val newSet = if (newAllSelectedValue) {
+                DayOfWeek.entries.toSet()
+            } else {
+                emptySet()
+            }
+
+            state.copy(
+                selectedDayOfWeek = newSet,
+                isAllDaysSelected = newAllSelectedValue
+            )
+        }
+    }
+
+    fun onSelectDayOfWeek(dayOfWeek: DayOfWeek){
+        _uiState.update { state ->
+            val currentSet = state.selectedDayOfWeek
+
+            val updatedSet = if (currentSet.contains(dayOfWeek)) {
+                currentSet - dayOfWeek
+            } else {
+                currentSet + dayOfWeek
+            }
+
+            state.copy(
+                selectedDayOfWeek = updatedSet,
+                isAllDaysSelected = updatedSet.size == 7
+            )
+        }
+    }
+
     fun onAddAlarmClock() {
         viewModelScope.launch {
             val formater = dataTimeFormatter.convert12To24Hour(_uiState.value.selectedHours, _uiState.value.selectedAmPm)
@@ -115,7 +155,10 @@ class SettingsAlarmViewModel @Inject constructor(
             addAlarmUseCase(Alarm(
                 id = if (alarmId == -1) 0 else alarmId,
                 hours = formater,
-                minutes = _uiState.value.selectedMinutes
+                minutes = _uiState.value.selectedMinutes,
+                repeatDays = _uiState.value.selectedDayOfWeek,
+                isRepeated = _uiState.value.selectedDayOfWeek.isNotEmpty(),
+                taskId = 1
             ))
         }
     }
