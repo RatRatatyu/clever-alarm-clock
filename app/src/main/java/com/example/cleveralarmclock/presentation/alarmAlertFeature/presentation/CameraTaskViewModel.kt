8b44b347b7b6efb4cd0c3ec7bb1c4b22acbf8bbc
Margarita <kkom.margarita@gmail.com>
2026-08-01 18:03:01 +0500

    feat: update stopMusic in CameraTaskViewModel to fetch alarm by ID before stopping

--- app/src/main/java/com/example/cleveralarmclock/presentation/alarmAlertFeature/presentation/CameraTaskViewModel.kt
+++ app/src/main/java/com/example/cleveralarmclock/presentation/alarmAlertFeature/presentation/CameraTaskViewModel.kt
@@ -2,20 +2,30 @@ package com.example.cleveralarmclock.presentation.alarmAlertFeature.presentation
 
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.cleveralarmclock.core.domain.usecase.GetAlarmByIdUseCase
 import com.example.cleveralarmclock.core.domain.usecase.StopAlarmUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class CameraTaskViewModel @Inject constructor(
     savedStateHandle: SavedStateHandle,
-    private val stopAlarmUseCase: StopAlarmUseCase
+    private val stopAlarmUseCase: StopAlarmUseCase,
+    private val getAlarmByIdUseCase: GetAlarmByIdUseCase
 ): ViewModel(){
 
     val alarmId: Int = savedStateHandle.get<Int>("ALARM_ID") ?: -1
 
+
+
     fun stopMusic(){
-        stopAlarmUseCase()
+        viewModelScope.launch {
+            val alarmById = getAlarmByIdUseCase(alarmId)
+            stopAlarmUseCase(alarmById!!)
+        }
+
     }
 
 }
