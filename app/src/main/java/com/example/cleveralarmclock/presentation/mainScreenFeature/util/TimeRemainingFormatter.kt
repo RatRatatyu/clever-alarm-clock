80ddcb3c8a50a5fd005d8fee75fa37cf42bebdfb
Margarita <kkom.margarita@gmail.com>
2026-08-01 18:01:15 +0500

    feat: update TimeRemainingFormatter to include days in the formatted string

--- app/src/main/java/com/example/cleveralarmclock/presentation/mainScreenFeature/util/TimeRemainingFormatter.kt
+++ app/src/main/java/com/example/cleveralarmclock/presentation/mainScreenFeature/util/TimeRemainingFormatter.kt
@@ -12,11 +12,22 @@ class TimeRemainingFormatter @Inject constructor(
 ) {
     fun format(hours: Long, minutes: Long): String {
         val res = context.resources
-        val hoursString = res.getQuantityString(R.plurals.hours_count, hours.toInt(), hours.toInt())
+
+        val days = hours / 24
+        val remainingHours = hours % 24
+
+        val daysString = res.getQuantityString(R.plurals.days_count, days.toInt(), days.toInt())
+        val hoursString = res.getQuantityString(R.plurals.hours_count, remainingHours.toInt(), remainingHours.toInt())
         val minutesString = res.getQuantityString(R.plurals.minutes_count, minutes.toInt(), minutes.toInt())
 
         return when {
             hours == 0L && minutes == 0L -> context.getString(R.string.will_ring_in_less_than_a_minute)
+            days > 0L -> {
+                when {
+                    remainingHours == 0L -> context.getString(R.string.will_ring_in_format, daysString, "").trim()
+                    else -> context.getString(R.string.will_ring_in_format, daysString, hoursString).trim()
+                }
+            }
             hours == 0L -> context.getString(R.string.will_ring_in_format, "", minutesString).trim()
             minutes == 0L -> context.getString(R.string.will_ring_in_format, hoursString, "").trim()
             else -> context.getString(R.string.will_ring_in_format, hoursString, minutesString)
