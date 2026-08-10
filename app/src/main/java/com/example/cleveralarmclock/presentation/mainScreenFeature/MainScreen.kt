a26b933a94a0fc9cbfc7c96badba94459e0f111c
Margarita <kkom.margarita@gmail.com>
2026-08-10 23:28:55 +0500

    feat: add MainScreenExpanded and update MainScreen to support adaptive layouts

--- app/src/main/java/com/example/cleveralarmclock/presentation/mainScreenFeature/MainScreen.kt
+++ app/src/main/java/com/example/cleveralarmclock/presentation/mainScreenFeature/MainScreen.kt
@@ -1,9 +1,12 @@
 package com.example.cleveralarmclock.presentation.mainScreenFeature
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -22,9 +25,12 @@ import androidx.compose.material3.IconButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.windowsizeclass.WindowSizeClass
+import androidx.compose.material3.windowsizeclass.WindowWidthSizeClass
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Devices.PHONE
@@ -38,11 +44,11 @@ import com.example.cleveralarmclock.presentation.mainScreenFeature.components.Al
 import java.time.DayOfWeek
 
 
-
 @Composable
 fun MainScreen(
     modifier: Modifier = Modifier,
     onAddAlarmClick: (Int) -> Unit,
+    windowSizeClass: WindowSizeClass,
     viewModel: MainViewModel = hiltViewModel(),
 ) {
 
@@ -56,23 +62,136 @@ fun MainScreen(
         }
     }
 
-    MainScreenCompact(
-        modifier = modifier,
-        scheduleList = scheduleList,
-        nextAlarmText = nextAlarmText,
-        uiState = uiState,
-        onAddAlarmClick = onAddAlarmClick,
-        clearSelection = { viewModel.clearSelection() },
-        getAllChecked = { viewModel.getAllChecked() },
-        deleteAlarms = { viewModel.deleteAlarms() },
-        toggleAlarmStatus = { viewModel.toggleAlarmStatus(it) },
-        onPress = { viewModel.onPress(it) },
-        onLongPress = { viewModel.onLongPress(it) }
-    )
+    when(windowSizeClass.widthSizeClass){
+        WindowWidthSizeClass.Compact -> MainScreenCompact(
+            modifier = modifier,
+            scheduleList = scheduleList,
+            nextAlarmText = nextAlarmText,
+            uiState = uiState,
+            onAddAlarmClick = onAddAlarmClick,
+            clearSelection = { viewModel.clearSelection() },
+            getAllChecked = { viewModel.getAllChecked() },
+            deleteAlarms = { viewModel.deleteAlarms() },
+            toggleAlarmStatus = { viewModel.toggleAlarmStatus(it) },
+            onPress = { viewModel.onPress(it) },
+            onLongPress = { viewModel.onLongPress(it) }
+        )
 
-
+        WindowWidthSizeClass.Medium,
+        WindowWidthSizeClass.Expanded -> MainScreenExpanded(
+            modifier = modifier,
+            scheduleList = scheduleList,
+            nextAlarmText = nextAlarmText,
+            uiState = uiState,
+            onAddAlarmClick = onAddAlarmClick,
+            clearSelection = { viewModel.clearSelection() },
+            getAllChecked = { viewModel.getAllChecked() },
+            deleteAlarms = { viewModel.deleteAlarms() },
+            toggleAlarmStatus = { viewModel.toggleAlarmStatus(it) },
+            onPress = { viewModel.onPress(it) },
+            onLongPress = { viewModel.onLongPress(it) }
+        )
+    }
 }
 
+
+@Composable
+fun MainScreenExpanded(
+    modifier: Modifier = Modifier,
+    scheduleList: List<AlarmUiModel>,
+    nextAlarmText: String?,
+    uiState: MainState,
+    onAddAlarmClick: (Int) -> Unit,
+    clearSelection: () -> Unit,
+    getAllChecked: () -> Unit,
+    deleteAlarms: () -> Unit,
+    toggleAlarmStatus: (Int) -> Unit,
+    onPress: (Int) -> Unit,
+    onLongPress: (Int) -> Unit
+){
+    Scaffold(
+        modifier = modifier
+            .fillMaxSize(),
+        topBar = {
+            if (uiState.isSelectedMode) {
+                TopAppBarComponent(
+                    clearSelection = clearSelection,
+                    getAllChecked = getAllChecked
+                )
+            }
+        },
+    ) { innerPadding ->
+
+        Row (
+            modifier
+                .fillMaxSize()
+                .padding(innerPadding),
+            horizontalArrangement = Arrangement.Center
+        ){
+            Column(
+                modifier = Modifier
+                    .weight(2f)
+            ) {
+                Box(
+                    Modifier
+                        .fillMaxSize()
+                        .weight(1f)
+                ){
+                    AlarmList(
+                        scheduleList = scheduleList,
+                        nextAlarmText = nextAlarmText,
+                        isSelectedMode = uiState.isSelectedMode,
+                        isChecked = uiState.selectedList,
+                        toggleAlarmStatus = {alarmId -> toggleAlarmStatus(alarmId)},
+                        onPress = {alarmId -> onPress(alarmId)},
+                        onLongPress = {alarmId -> onLongPress(alarmId)}
+                    )
+                }
+            }
+            Column(
+                modifier = Modifier
+                    .fillMaxHeight()
+                    .weight(1f),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ) {
+                if(uiState.isSelectedMode){
+                    FilledIconButton(
+                        onClick = { deleteAlarms() },
+                        modifier = Modifier.size(56.dp),
+                        colors = IconButtonDefaults.filledIconButtonColors(
+                            containerColor = MaterialTheme.colorScheme.errorContainer,
+                            contentColor = MaterialTheme.colorScheme.error
+                        )
+                    ) {
+                        Icon(
+                            imageVector = Icons.Default.Delete,
+                            contentDescription = stringResource(R.string.delete),
+                            modifier = Modifier.size(28.dp)
+                        )
+                    }
+                }else{
+                    FilledIconButton(
+                        onClick = { onAddAlarmClick(-1) },
+                        modifier = Modifier.size(56.dp),
+                        colors = IconButtonDefaults.filledIconButtonColors(
+                            containerColor = MaterialTheme.colorScheme.primary
+                        )
+                    ) {
+                        Icon(
+                            imageVector = Icons.Default.Add,
+                            contentDescription = "Add alarm clock"
+                        )
+                    }
+                }
+
+
+            }
+
+
+        }
+    }
+}
 
 @Composable
 fun MainScreenCompact(
