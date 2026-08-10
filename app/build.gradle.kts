ef5515a3febece3a0b4172a1443fc9fff4c8989a
Margarita <kkom.margarita@gmail.com>
2026-08-10 15:50:49 +0500

    build: add Compose Material3 windowSizeClass dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,6 +43,7 @@ android {
 
 
 dependencies {
+    implementation(libs.androidx.compose.material3.windowSizeClass)
     implementation(libs.androidx.room.runtime)
     implementation(libs.androidx.room.ktx)
     ksp(libs.androidx.room.compiler)
