3d7263b323f462f5fb6bf694162e5a92077c967d
Margarita <kkom.margarita@gmail.com>
2026-08-12 16:47:18 +0500

    feat: add AlarmTaskPicker component for selectable alarm tasks in a grid layout

---
+++ app/src/main/java/com/example/cleveralarmclock/presentation/manageAlarmFeature/components/AlarmTaskPicker.kt
@@ -0,0 +1,101 @@
+package com.example.cleveralarmclock.presentation.manageAlarmFeature.components
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.example.cleveralarmclock.core.domain.task.AlarmTask
+import com.example.cleveralarmclock.core.domain.task.TaskType
+
+
+@Composable
+fun AlarmTaskPicker(
+    modifier: Modifier = Modifier,
+    alarmTaskList: List<AlarmTask>,
+    selectedTask: TaskType,
+    onTaskSelected: (TaskType) -> Unit
+){
+    LazyVerticalGrid(
+        columns = GridCells.Fixed(2),
+        horizontalArrangement = Arrangement.spacedBy(12.dp),
+        verticalArrangement = Arrangement.spacedBy(12.dp),
+        contentPadding = PaddingValues(horizontal = 16.dp, vertical = 8.dp),
+        modifier = modifier.fillMaxWidth()
+    ){
+
+
+        items(alarmTaskList, key = {it.type}){ task ->
+            val isSelected = selectedTask == task.type
+
+            val backgroundColor = if (isSelected) {
+                MaterialTheme.colorScheme.primary
+            } else {
+                MaterialTheme.colorScheme.surfaceVariant
+            }
+
+            val textColor = if (isSelected) {
+                MaterialTheme.colorScheme.onPrimary
+            } else {
+                MaterialTheme.colorScheme.onSurfaceVariant
+            }
+
+            Card(
+                onClick = {onTaskSelected(task.type)},
+                shape = RoundedCornerShape(16.dp),
+                colors = CardDefaults.cardColors(
+                    containerColor = backgroundColor,
+                    contentColor = textColor
+                ),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .aspectRatio(1.5f)
+            ) {
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(12.dp),
+                    verticalArrangement = Arrangement.SpaceBetween
+                ) {
+                    Row(
+                        modifier = Modifier.fillMaxWidth(),
+                        horizontalArrangement = Arrangement.spacedBy(8.dp),
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Icon(
+                            painter = painterResource(id = task.iconResId),
+                            contentDescription = stringResource(id = task.titleResId),)
+
+                        Text(
+                            text = stringResource(id = task.titleResId),
+                            style = MaterialTheme.typography.titleMedium,)
+                    }
+
+                    Text(
+                        text = stringResource(id = task.descriptionResId),
+                        style = MaterialTheme.typography.bodySmall,
+                    )
+                }
+
+            }
+        }
+    }
+}
