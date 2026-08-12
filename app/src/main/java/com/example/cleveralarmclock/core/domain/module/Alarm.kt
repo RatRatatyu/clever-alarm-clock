41c76dfc186be312042948e01611789783ada10b
Margarita <kkom.margarita@gmail.com>
2026-08-12 17:12:13 +0500

    refactor: update Alarm data class to use TaskType for taskId

--- app/src/main/java/com/example/cleveralarmclock/core/domain/module/Alarm.kt
+++ app/src/main/java/com/example/cleveralarmclock/core/domain/module/Alarm.kt
@@ -1,12 +1,13 @@
 package com.example.cleveralarmclock.core.domain.module
 
+import com.example.cleveralarmclock.core.domain.task.TaskType
 import java.time.DayOfWeek
 
 data class Alarm (
     val id: Int = 0,
     val hours: Int,
     val minutes: Int,
-    val taskId: Int,
+    val taskId: TaskType,
     val repeatDays: Set<DayOfWeek>,
     val isRepeated: Boolean = true,
     val isActivate: Boolean = true
