3ea82c4101d69d4471746b0d0926b10c5164a8d5
Margarita <kkom.margarita@gmail.com>
2026-08-13 21:00:59 +0500

    refactor: update AlarmAlertActivity to use AlarmNavHost and support alarmId intent extra

--- app/src/main/java/com/example/cleveralarmclock/presentation/alarmAlertFeature/AlarmAlertActivity.kt
+++ app/src/main/java/com/example/cleveralarmclock/presentation/alarmAlertFeature/AlarmAlertActivity.kt
@@ -6,26 +6,30 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import com.example.cleveralarmclock.presentation.alarmAlertFeature.presentation.CameraTaskFeature
+import com.example.cleveralarmclock.presentation.alarmAlertFeature.presentation.navigation.AlarmNavHost
+import com.example.cleveralarmclock.ui.theme.CleverAlarmClockTheme
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class AlarmAlertActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState) //!!!!
+        super.onCreate(savedInstanceState)
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O_MR1) {
             setShowWhenLocked(true)
             setTurnScreenOn(true)
         }
 
-
         val keyguardManager = getSystemService(KeyguardManager::class.java)
         keyguardManager?.requestDismissKeyguard(this, null)
 
+        val alarmId = intent.getIntExtra("ALARM_ID", -1)
+
         enableEdgeToEdge()
         setContent {
-            CameraTaskFeature()
+            CleverAlarmClockTheme {
+                AlarmNavHost(alarmId = alarmId)
+            }
         }
     }
 }
