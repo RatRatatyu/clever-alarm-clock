0067856b68f6fee2abaa0d8fd365f43787dcfe4a
Margarita <kkom.margarita@gmail.com>
2026-08-13 21:01:34 +0500

    refactor: rename CameraTaskFeature to CameraTaskScreen and move to cameraTaskFeature package

---
+++ app/src/main/java/com/example/cleveralarmclock/presentation/alarmAlertFeature/presentation/cameraTaskFeature/CameraTaskScreen.kt
@@ -0,0 +1,27 @@
+package com.example.cleveralarmclock.presentation.alarmAlertFeature.presentation.cameraTaskFeature
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.hilt.navigation.compose.hiltViewModel
+
+@Composable
+fun CameraTaskScreen(
+    modifier: Modifier = Modifier,
+    viewModel: CameraTaskViewModel = hiltViewModel()
+){
+    Column(
+        modifier
+            .fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text("This is screen task for CAMERA")
+        Button(onClick = { viewModel.stopMusic() }) { Text("stop") }
+    }
+}
