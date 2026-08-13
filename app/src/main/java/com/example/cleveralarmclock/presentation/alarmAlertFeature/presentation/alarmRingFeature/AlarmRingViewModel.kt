72f6211d8068919ecdcbf304e7778c995302dd76
Margarita <kkom.margarita@gmail.com>
2026-08-13 21:01:25 +0500

    feat: add AlarmRingViewModel to handle alarm data loading and task navigation events

---
+++ app/src/main/java/com/example/cleveralarmclock/presentation/alarmAlertFeature/presentation/alarmRingFeature/AlarmRingViewModel.kt
@@ -0,0 +1,68 @@
+package com.example.cleveralarmclock.presentation.alarmAlertFeature.presentation.alarmRingFeature
+
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.cleveralarmclock.core.domain.task.TaskType
+import com.example.cleveralarmclock.core.domain.usecase.GetAlarmByIdUseCase
+import dagger.hilt.android.lifecycle.HiltViewModel
+import jakarta.inject.Inject
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.receiveAsFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+
+
+data class AlarmRingUiState(
+    val isLoading: Boolean = false,
+    val taskId: TaskType = TaskType.SHAKE
+
+)
+
+@HiltViewModel
+class AlarmRingViewModel @Inject constructor(
+    savedStateHandle: SavedStateHandle,
+    private val getAlarmByIdUseCase: GetAlarmByIdUseCase,
+): ViewModel() {
+
+    private val alarmId: Int = checkNotNull(savedStateHandle["alarmId"])
+
+    private val _uiState = MutableStateFlow(AlarmRingUiState())
+    val uiState: StateFlow<AlarmRingUiState> = _uiState.asStateFlow()
+    private val _navigationEvent = Channel<TaskType>()
+    val navigationEvent = _navigationEvent.receiveAsFlow()
+
+    init {
+        loadAlarmInfo()
+    }
+
+    private fun loadAlarmInfo(){
+        _uiState.update { it.copy(
+            isLoading = true
+        ) }
+        viewModelScope.launch {
+            try {
+                if(alarmId != -1){
+                    val alarm = getAlarmByIdUseCase(alarmId)
+                    _uiState.update { it.copy(
+                        taskId = alarm?.taskId ?: TaskType.SHAKE
+                    ) }
+                }
+            } finally {
+                _uiState.update { it.copy(
+                    isLoading = false
+                ) }
+            }
+        }
+    }
+
+    fun onStopAlarm(){
+        viewModelScope.launch {
+            _navigationEvent.send(_uiState.value.taskId)
+        }
+    }
+
+}
