82f4677c1cd77be7a36fbae7cffde05e4595c0bd
Margarita <kkom.margarita@gmail.com>
2026-08-15 01:23:41 +0500

    refactor: update AlarmRingScreen to support snooze action and activity dismissal

--- app/src/main/java/com/example/cleveralarmclock/presentation/alarmAlertFeature/presentation/alarmRingFeature/AlarmRingScreen.kt
+++ app/src/main/java/com/example/cleveralarmclock/presentation/alarmAlertFeature/presentation/alarmRingFeature/AlarmRingScreen.kt
@@ -1,5 +1,6 @@
 package com.example.cleveralarmclock.presentation.alarmAlertFeature.presentation.alarmRingFeature
 
+import android.app.Activity
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -13,8 +14,11 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.example.cleveralarmclock.R
 import com.example.cleveralarmclock.core.domain.task.TaskType
 
 
@@ -24,9 +28,17 @@ fun AlarmRingScreen(
     viewModel: AlarmRingViewModel = hiltViewModel(),
     onStopAlarm: (TaskType) -> Unit
 ) {
+    val context = LocalContext.current
+    val activity = context as? Activity
+
     LaunchedEffect(Unit) {
         viewModel.navigationEvent.collect { taskId ->
             onStopAlarm(taskId)
+        }
+    }
+    LaunchedEffect(Unit) {
+        viewModel.closeActivityEvent.collect {
+            activity?.finish()
         }
     }
 
@@ -49,8 +61,8 @@ fun AlarmRingScreen(
                 CircularProgressIndicator()
             }else{
 
-                Button(onClick = {}) { // will be implemented later
-                    Text("Delay for 5 minutes")
+                Button(onClick = {viewModel.snoozeAlarm()}) {
+                    Text(stringResource(R.string.snoozing_for_10_minutes))
                 }
 
                 Button(onClick = { viewModel.onStopAlarm() }) {
