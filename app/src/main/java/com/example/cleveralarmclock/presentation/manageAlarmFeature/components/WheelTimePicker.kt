da9967b287df838589e9e657d2c12624c49d6628
Margarita <kkom.margarita@gmail.com>
2026-08-14 17:56:20 +0500

    refactor: update WheelTimePicker to use MaterialTheme color for selection background

--- app/src/main/java/com/example/cleveralarmclock/presentation/manageAlarmFeature/components/WheelTimePicker.kt
+++ app/src/main/java/com/example/cleveralarmclock/presentation/manageAlarmFeature/components/WheelTimePicker.kt
@@ -18,7 +18,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import com.example.cleveralarmclock.core.ui.WheelPicker
 import java.util.Locale
@@ -86,7 +85,7 @@ fun WheelTimePicker(
                     .fillMaxWidth()
                     .height(45.dp)
                     .clip(RoundedCornerShape(10.dp))
-                    .background(Color.Gray.copy(alpha = 0.15f))
+                    .background(MaterialTheme.colorScheme.secondaryContainer)
             )
 
             Row (
