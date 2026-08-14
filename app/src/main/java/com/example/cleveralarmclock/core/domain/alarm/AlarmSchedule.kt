9cbcc9223a74472272b4dadd58d1371da05a60ff
Margarita <kkom.margarita@gmail.com>
2026-08-15 01:23:58 +0500

    refactor: update AlarmSchedule to include snoozeFor10seconds method

--- app/src/main/java/com/example/cleveralarmclock/core/domain/alarm/AlarmSchedule.kt
+++ app/src/main/java/com/example/cleveralarmclock/core/domain/alarm/AlarmSchedule.kt
@@ -4,5 +4,6 @@ import java.time.DayOfWeek
 
 interface AlarmSchedule{
     fun schedule(hour: Int, minute: Int, repeatDays: Set<DayOfWeek>, id: Int){}
+    fun snoozeFor10seconds(id: Int){}
     fun cancel(id: Int){}
 }
