e29646faad3406f7116a2aeeb234e83ffade3be9
Margarita <kkom.margarita@gmail.com>
2026-08-16 23:06:44 +0500

    refactor: add RescheduleNextAlarmUseCase to handle alarm rescheduling and toggling logic

---
+++ app/src/main/java/com/example/cleveralarmclock/core/domain/usecase/RescheduleNextAlarmUseCase.kt
@@ -0,0 +1,25 @@
+package com.example.cleveralarmclock.core.domain.usecase
+
+import com.example.cleveralarmclock.core.domain.alarm.AlarmSchedule
+import javax.inject.Inject
+
+class RescheduleNextAlarmUseCase @Inject constructor(
+    private val getAlarmByIdUseCase: GetAlarmByIdUseCase,
+    private val alarmSchedule: AlarmSchedule,
+    private val toggleAlarmUseCase: ToggleAlarmUseCase
+){
+    suspend operator fun invoke(alarmId: Int){
+        val alarm = getAlarmByIdUseCase(alarmId) ?: return
+
+        if(alarm.isRepeated){
+            alarmSchedule.schedule(
+                alarm.hours,
+                alarm.minutes,
+                alarm.repeatDays,
+                alarm.id)
+
+        }else{
+            toggleAlarmUseCase(alarm.id)
+        }
+    }
+}
