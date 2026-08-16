4e7247b7124583cf29468199682890abdc4b7475
Margarita <kkom.margarita@gmail.com>
2026-08-16 23:07:02 +0500

    refactor: rename StopAlarmUseCase to StopAlarmPlayerUseCase and remove alarm scheduling logic

---
+++ app/src/main/java/com/example/cleveralarmclock/core/domain/usecase/StopAlarmPlayerUseCase.kt
@@ -0,0 +1,21 @@
+package com.example.cleveralarmclock.core.domain.usecase
+
+import android.content.Context
+import android.content.Intent
+import com.example.cleveralarmclock.core.domain.alarm.AlarmPlayer
+import com.example.cleveralarmclock.core.service.services.AlarmService
+import dagger.hilt.android.qualifiers.ApplicationContext
+import javax.inject.Inject
+
+class StopAlarmPlayerUseCase @Inject constructor(
+    @param:ApplicationContext private val context: Context,
+    private val alarmPlayer: AlarmPlayer,
+) {
+
+     operator fun invoke(alarmId: Int){
+        alarmPlayer.stopPlayer()
+
+        val intent = Intent(context, AlarmService::class.java)
+        context.stopService(intent)
+    }
+}
