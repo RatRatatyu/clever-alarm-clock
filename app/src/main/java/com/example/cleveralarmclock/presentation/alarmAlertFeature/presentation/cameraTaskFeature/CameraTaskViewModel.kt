c4f6d251b05b6dc4001adfa05ed4ec56de825c03
Margarita <kkom.margarita@gmail.com>
2026-08-16 23:06:06 +0500

    refactor: update CameraTaskViewModel to use StopAlarmPlayerUseCase

--- app/src/main/java/com/example/cleveralarmclock/presentation/alarmAlertFeature/presentation/cameraTaskFeature/CameraTaskViewModel.kt
+++ app/src/main/java/com/example/cleveralarmclock/presentation/alarmAlertFeature/presentation/cameraTaskFeature/CameraTaskViewModel.kt
@@ -3,8 +3,7 @@ package com.example.cleveralarmclock.presentation.alarmAlertFeature.presentation
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.example.cleveralarmclock.core.domain.usecase.GetAlarmByIdUseCase
-import com.example.cleveralarmclock.core.domain.usecase.StopAlarmUseCase
+import com.example.cleveralarmclock.core.domain.usecase.StopAlarmPlayerUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
 import javax.inject.Inject
@@ -12,7 +11,7 @@ import javax.inject.Inject
 @HiltViewModel
 class CameraTaskViewModel @Inject constructor(
     savedStateHandle: SavedStateHandle,
-    private val stopAlarmUseCase: StopAlarmUseCase,
+    private val stopAlarmUseCase: StopAlarmPlayerUseCase,
 ): ViewModel() {
 
     private val alarmId: Int = checkNotNull(savedStateHandle["alarmId"])
