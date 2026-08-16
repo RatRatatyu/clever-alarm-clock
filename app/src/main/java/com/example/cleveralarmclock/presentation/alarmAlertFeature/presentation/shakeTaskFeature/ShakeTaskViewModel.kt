e3bf1822881a7b86ea0fa3f52582746761aaac8a
Margarita <kkom.margarita@gmail.com>
2026-08-16 23:06:53 +0500

    refactor: update ShakeTaskViewModel to use StopAlarmPlayerUseCase

--- app/src/main/java/com/example/cleveralarmclock/presentation/alarmAlertFeature/presentation/shakeTaskFeature/ShakeTaskViewModel.kt
+++ app/src/main/java/com/example/cleveralarmclock/presentation/alarmAlertFeature/presentation/shakeTaskFeature/ShakeTaskViewModel.kt
@@ -3,7 +3,7 @@ package com.example.cleveralarmclock.presentation.alarmAlertFeature.presentation
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.example.cleveralarmclock.core.domain.usecase.StopAlarmUseCase
+import com.example.cleveralarmclock.core.domain.usecase.StopAlarmPlayerUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
 import javax.inject.Inject
@@ -12,7 +12,7 @@ import javax.inject.Inject
 @HiltViewModel
 class ShakeTaskViewModel @Inject constructor(
     savedStateHandle: SavedStateHandle,
-    private val stopAlarmUseCase: StopAlarmUseCase,
+    private val stopAlarmUseCase: StopAlarmPlayerUseCase,
 ): ViewModel() {
 
     private val alarmId: Int = checkNotNull(savedStateHandle["alarmId"])
