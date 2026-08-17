ee43367dd0bf8ab4ef42c60ab1f67d1fa047e4b8
Margarita <kkom.margarita@gmail.com>
2026-08-17 23:37:18 +0500

    refactor: add DismissTodayUseCase to handle alarm rescheduling and cancellation

---
+++ app/src/main/java/com/example/cleveralarmclock/core/domain/usecase/DismissTodayUseCase.kt
@@ -0,0 +1,14 @@
+package com.example.cleveralarmclock.core.domain.usecase
+
+import com.example.cleveralarmclock.core.domain.alarm.AlarmSchedule
+import javax.inject.Inject
+
+class DismissTodayUseCase @Inject constructor(
+    private val rescheduleNextAlarmUseCase: RescheduleNextAlarmUseCase,
+    private val alarmSchedule: AlarmSchedule
+) {
+    suspend operator fun invoke(id: Int) {
+        rescheduleNextAlarmUseCase(id)
+        alarmSchedule.cancel(id)
+    }
+}
