99f964f4da61d99c94d2e772849d8381862991a7
Margarita <kkom.margarita@gmail.com>
2026-08-17 23:37:08 +0500

    refactor: add DisableAlarmUseCase to handle alarm deactivation and cancellation logic

---
+++ app/src/main/java/com/example/cleveralarmclock/core/domain/usecase/DisableAlarmUseCase.kt
@@ -0,0 +1,19 @@
+package com.example.cleveralarmclock.core.domain.usecase
+
+import com.example.cleveralarmclock.core.domain.alarm.AlarmSchedule
+import com.example.cleveralarmclock.core.domain.repository.AlarmRepository
+import javax.inject.Inject
+
+class DisableAlarmUseCase @Inject constructor(
+    private val alarmRepository: AlarmRepository,
+    private val alarmSchedule: AlarmSchedule
+) {
+    suspend operator fun invoke(id: Int) {
+        val alarm = alarmRepository.getAlarmById(id) ?: return
+
+        val updatedAlarm = alarm.copy(isActivate = !alarm.isActivate)
+        alarmRepository.updateAlarm(updatedAlarm)
+
+        alarmSchedule.cancel(id)
+    }
+}
