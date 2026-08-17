8b4e543eaa56719d2043a2155b662997eec7e790
Margarita <kkom.margarita@gmail.com>
2026-08-17 23:38:22 +0500

    feat: add PreNotificationReceiver to handle pre-alarm notifications and dismissal actions

---
+++ app/src/main/java/com/example/cleveralarmclock/core/service/receivers/PreNotificationReceiver.kt
@@ -0,0 +1,63 @@
+package com.example.cleveralarmclock.core.service.receivers
+
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import com.example.cleveralarmclock.core.domain.util.toPreNotificationId
+import com.example.cleveralarmclock.core.notifications.NotificationFactory
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
+
+@AndroidEntryPoint
+class PreNotificationReceiver: BroadcastReceiver(){
+
+    @Inject
+    lateinit var notificationFactory: NotificationFactory
+
+    override fun onReceive(context: Context?, intent: Intent?) {
+        val ctx = context ?: return
+
+        val alarmId = intent?.getIntExtra("ALARM_ID", -1) ?: -1
+        if (alarmId == -1) return
+
+        val preNotificationId = alarmId.toPreNotificationId()
+
+        val dismissIntent = Intent(context, AlarmActionReceiver::class.java).apply {
+            action = ACTION_DISMISS_TODAY
+            putExtra("ALARM_ID", alarmId)
+        }
+        val dismissPendingIntent = PendingIntent.getBroadcast(
+            context,
+            preNotificationId.plus(200),
+            dismissIntent,
+            PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
+        )
+
+        val turnOffIntent = Intent(context, AlarmActionReceiver::class.java).apply {
+            action = ACTION_TURN_OFF_PERMANENTLY
+            putExtra("ALARM_ID", alarmId)
+        }
+        val turnOffPendingIntent = PendingIntent.getBroadcast(
+            context,
+            preNotificationId.plus(300),
+            turnOffIntent,
+            PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
+        )
+
+        val notification = notificationFactory.createPreNotification(
+            dismissPendingIntent,
+            turnOffPendingIntent
+        )
+
+        val notificationManager = ctx.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.notify(preNotificationId, notification)
+    }
+
+    companion object {
+        const val ACTION_DISMISS_TODAY = "com.example.cleveralarmclock.ACTION_DISMISS_TODAY"
+        const val ACTION_TURN_OFF_PERMANENTLY = "com.example.cleveralarmclock.ACTION_TURN_OFF_PERMANENTLY"
+    }
+
+}
