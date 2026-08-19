4b2ed11082e5c4829b9488dcd1e0c5d59e4321b1
Margarita <kkom.margarita@gmail.com>
2026-08-19 16:59:02 +0500

    refactor: replace fillMaxSize with fillMaxWidth and vertical padding in WheelTimePicker

--- app/src/main/java/com/example/cleveralarmclock/presentation/manageAlarmFeature/components/WheelTimePicker.kt
+++ app/src/main/java/com/example/cleveralarmclock/presentation/manageAlarmFeature/components/WheelTimePicker.kt
@@ -5,7 +5,6 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -77,7 +76,9 @@ fun WheelTimePicker(
         }
 
         Box (
-            modifier = Modifier.fillMaxSize(),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = 10.dp),
             contentAlignment = Alignment.Center
         ) {
             Box(
