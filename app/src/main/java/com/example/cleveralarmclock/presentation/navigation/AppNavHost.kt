071c26800ecd9ea9ebcd3b41a19e6ddb047062a4
Margarita <kkom.margarita@gmail.com>
2026-08-20 00:35:42 +0500

    feat: update AppNavHost to show AlarmSettings as a dialog on non-compact screens

--- app/src/main/java/com/example/cleveralarmclock/presentation/navigation/AppNavHost.kt
+++ app/src/main/java/com/example/cleveralarmclock/presentation/navigation/AppNavHost.kt
@@ -1,11 +1,14 @@
 package com.example.cleveralarmclock.presentation.navigation
 
 import androidx.compose.material3.windowsizeclass.WindowSizeClass
+import androidx.compose.material3.windowsizeclass.WindowWidthSizeClass
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.window.DialogProperties
 import androidx.navigation.NavHostController
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.compose.dialog
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navArgument
 import com.example.cleveralarmclock.presentation.mainScreenFeature.MainScreen
@@ -25,6 +28,8 @@ fun AppNavHost(
     navController: NavHostController = rememberNavController(),
     windowSizeClass: WindowSizeClass
 ){
+    val isCompact = windowSizeClass.widthSizeClass == WindowWidthSizeClass.Compact
+
     NavHost(
         navController = navController,
         startDestination = AppScreens.Home.route
@@ -37,18 +42,35 @@ fun AppNavHost(
                 windowSizeClass = windowSizeClass
             )
         }
-        composable(
-            route = AppScreens.AlarmSettings.route,
-            arguments= listOf(
-                navArgument("alarmId"){type = NavType.IntType}
-            )
-        ){
-            SettingsAlarm(
-                onBackClick = {
-                    navController.popBackStack()
-                },
-                windowSizeClass = windowSizeClass
-            )
+        if(isCompact){
+            composable(
+                route = AppScreens.AlarmSettings.route,
+                arguments= listOf(
+                    navArgument("alarmId"){type = NavType.IntType}
+                )
+            ){
+                SettingsAlarm(
+                    onBackClick = {
+                        navController.popBackStack()
+                    },
+                    windowSizeClass = windowSizeClass
+                )
+            }
+        }else{
+            dialog(
+                route = AppScreens.AlarmSettings.route,
+                arguments = listOf(
+                    navArgument("alarmId"){type = NavType.IntType}
+                ),
+                dialogProperties = DialogProperties(usePlatformDefaultWidth = false)
+            ){
+                SettingsAlarm(
+                    onBackClick = {
+                        navController.popBackStack()
+                    },
+                    windowSizeClass = windowSizeClass
+                )
+            }
         }
     }
 }
