1bdb8e5b0bd7bd0dfa2fa87db44bb1be1ad7dc4e
Margarita <kkom.margarita@gmail.com>
2026-08-23 16:52:33 +0500

    refactor: move DeleteAlarmsUseCase to manadge subpackage

--- app/src/main/java/com/example/cleveralarmclock/core/domain/usecase/manadge/DeleteAlarmsUseCase.kt
+++ app/src/main/java/com/example/cleveralarmclock/core/domain/usecase/manadge/DeleteAlarmsUseCase.kt
@@ -1,4 +1,4 @@
-package com.example.cleveralarmclock.core.domain.usecase
+package com.example.cleveralarmclock.core.domain.usecase.manadge
 
 import com.example.cleveralarmclock.core.domain.alarm.AlarmSchedule
 import com.example.cleveralarmclock.core.domain.repository.AlarmRepository
