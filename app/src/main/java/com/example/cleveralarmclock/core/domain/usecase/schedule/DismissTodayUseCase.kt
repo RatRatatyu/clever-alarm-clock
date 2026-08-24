af729a550dd4c7a1b861cded7aec1b67a6dbe9d5
Margarita <kkom.margarita@gmail.com>
2026-08-24 23:28:22 +0500

    refactor: remove AlarmSchedule dependency and update RescheduleNextAlarmUseCase call in DismissTodayUseCase

--- app/src/main/java/com/example/cleveralarmclock/core/domain/usecase/schedule/DismissTodayUseCase.kt
+++ app/src/main/java/com/example/cleveralarmclock/core/domain/usecase/schedule/DismissTodayUseCase.kt
@@ -1,14 +1,13 @@
 package com.example.cleveralarmclock.core.domain.usecase.schedule
 
-import com.example.cleveralarmclock.core.domain.alarm.AlarmSchedule
 import javax.inject.Inject
 
 class DismissTodayUseCase @Inject constructor(
     private val rescheduleNextAlarmUseCase: RescheduleNextAlarmUseCase,
-    private val alarmSchedule: AlarmSchedule
 ) {
     suspend operator fun invoke(id: Int) {
-        rescheduleNextAlarmUseCase(id)
-        alarmSchedule.cancel(id)
+
+        rescheduleNextAlarmUseCase(id, true)
+
     }
 }
