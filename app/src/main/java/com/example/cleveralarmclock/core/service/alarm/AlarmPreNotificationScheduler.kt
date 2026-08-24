f688d47011d6e7ef165ceda7429d892d654dbe34
Margarita <kkom.margarita@gmail.com>
2026-08-24 23:27:27 +0500

    feat: update AlarmPreNotificationScheduler to handle immediate triggers and include repetition status

--- app/src/main/java/com/example/cleveralarmclock/core/service/alarm/AlarmPreNotificationScheduler.kt
+++ app/src/main/java/com/example/cleveralarmclock/core/service/alarm/AlarmPreNotificationScheduler.kt
@@ -4,8 +4,9 @@ import android.app.AlarmManager
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
+import android.util.Log
+import com.example.cleveralarmclock.core.domain.usecase.manage.GetAlarmByIdUseCase
 import com.example.cleveralarmclock.core.domain.util.toPreNotificationId
-import com.example.cleveralarmclock.core.notifications.NotificationFactory
 import com.example.cleveralarmclock.core.service.receivers.PreNotificationReceiver
 import dagger.hilt.android.qualifiers.ApplicationContext
 import java.time.LocalDateTime
@@ -14,39 +15,53 @@ import javax.inject.Inject
 
 class AlarmPreNotificationScheduler @Inject constructor(
     @param:ApplicationContext private val context: Context,
+    private val getAlarmByIdUseCase: GetAlarmByIdUseCase
 ){
 
     private val alarmManager = context.getSystemService(Context.ALARM_SERVICE) as? AlarmManager
 
-    fun preNotificationScheduler(alarmId: Int, alarmTime: LocalDateTime){
-        val preNotificationId = alarmId.toPreNotificationId()
-        val preTime = alarmTime.minusMinutes(10)
+    suspend fun preNotificationScheduler(alarmId: Int, alarmTime: LocalDateTime){
+        val now = LocalDateTime.now()
 
-        if (preTime.isBefore(LocalDateTime.now())) return
+        if (!alarmTime.isAfter(now)) return
+
+        val preNotificationId = alarmId.toPreNotificationId()
+        val idealPreTime = alarmTime.minusMinutes(10)
+
+        //If you have more than 10 minutes before your alarm, set it 10 minutes before.
+        //If less than 10 minutes, set it to the current moment (now) so that it appears immediately.
+        val actualTriggerTime = if (idealPreTime.isAfter(now)) {
+            idealPreTime
+        } else {
+            now
+        }
+
+        val alarm = getAlarmByIdUseCase(alarmId)?.isRepeated ?: false
 
         val intent = Intent(context, PreNotificationReceiver::class.java).apply {
             putExtra("ALARM_ID", alarmId)
+            putExtra("IS_REPEATED", alarm)
         }
 
         val pendingIntent = PendingIntent.getBroadcast(
             context,
             preNotificationId,
-            intent, PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
+            intent,
+            PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
         )
 
-        val triggerTime= preTime
+        val triggerTime= actualTriggerTime
             .atZone(ZoneId.systemDefault())
             .toInstant()
             .toEpochMilli()
+
+        Log.i("ALARM_DEBUG", "set preNotification $alarmId")
 
         alarmManager?.setExactAndAllowWhileIdle(
             AlarmManager.RTC_WAKEUP,
             triggerTime,
             pendingIntent
         )
-
-
-
     }
 
     fun cancelPreNotification(alarmId: Int){
