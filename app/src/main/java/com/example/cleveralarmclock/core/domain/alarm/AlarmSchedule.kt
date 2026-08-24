db5ca57df92fafb3b1afa81fc3320900b1d4b928
Margarita <kkom.margarita@gmail.com>
2026-08-24 23:27:35 +0500

    refactor: update AlarmSchedule to use suspend functions and add skipForToday parameter

--- app/src/main/java/com/example/cleveralarmclock/core/domain/alarm/AlarmSchedule.kt
+++ app/src/main/java/com/example/cleveralarmclock/core/domain/alarm/AlarmSchedule.kt
@@ -3,7 +3,13 @@ package com.example.cleveralarmclock.core.domain.alarm
 import java.time.DayOfWeek
 
 interface AlarmSchedule{
-    fun schedule(hour: Int, minute: Int, repeatDays: Set<DayOfWeek>, id: Int){}
-    fun snoozeFor10seconds(id: Int){}
-    fun cancel(id: Int){}
+    suspend fun schedule(
+        hour: Int,
+        minute: Int,
+        repeatDays: Set<DayOfWeek>,
+        id: Int,
+        skipForToday: Boolean = false
+    )
+    suspend fun snoozeFor10seconds(id: Int)
+    fun cancel(id: Int)
 }
