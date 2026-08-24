0d363a675c875e25c9987c65b5e2719671c2c33e
Margarita <kkom.margarita@gmail.com>
2026-08-24 23:28:50 +0500

    refactor: update RescheduleNextAlarmUseCase to support skipForToday and deactivate one-time alarms

--- app/src/main/java/com/example/cleveralarmclock/core/domain/usecase/schedule/RescheduleNextAlarmUseCase.kt
+++ app/src/main/java/com/example/cleveralarmclock/core/domain/usecase/schedule/RescheduleNextAlarmUseCase.kt
@@ -1,27 +1,33 @@
 package com.example.cleveralarmclock.core.domain.usecase.schedule
 
 import com.example.cleveralarmclock.core.domain.alarm.AlarmSchedule
+import com.example.cleveralarmclock.core.domain.repository.AlarmRepository
 import com.example.cleveralarmclock.core.domain.usecase.manage.GetAlarmByIdUseCase
-import com.example.cleveralarmclock.core.domain.usecase.manage.ToggleAlarmUseCase
 import javax.inject.Inject
 
 class RescheduleNextAlarmUseCase @Inject constructor(
     private val getAlarmByIdUseCase: GetAlarmByIdUseCase,
-    private val alarmSchedule: AlarmSchedule,
-    private val toggleAlarmUseCase: ToggleAlarmUseCase
+    private val alarmRepository: AlarmRepository,
+    private val alarmSchedule: AlarmSchedule
 ){
-    suspend operator fun invoke(alarmId: Int){
-        val alarm = getAlarmByIdUseCase(alarmId) ?: return
+     suspend operator fun invoke(alarmId: Int, skipForToday: Boolean = false){
+         val alarm = getAlarmByIdUseCase(alarmId) ?: return
 
-        if(alarm.isRepeated){
-            alarmSchedule.schedule(
-                alarm.hours,
-                alarm.minutes,
-                alarm.repeatDays,
-                alarm.id)
+         if(!alarm.isRepeated){
+             val updatedAlarm = alarm.copy(isActivate = false)
+             alarmRepository.updateAlarm(updatedAlarm)
+         }
 
-        }else{
-            toggleAlarmUseCase(alarm.id)
-        }
+         if(alarm.isRepeated){
+             alarmSchedule.schedule(
+                 alarm.hours,
+                 alarm.minutes,
+                 alarm.repeatDays,
+                 alarm.id,
+                 skipForToday
+             )
+         }else{
+             alarmSchedule.cancel(alarm.id)
+         }
     }
 }
