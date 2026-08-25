1bd6fcdd1c6553d647550ff82e31bd1728094b3d
Margarita <kkom.margarita@gmail.com>
2026-08-25 15:59:50 +0500

    refactor: update RescheduleNextAlarmUseCase to use AlarmTimeCalculator for calculating next trigger time

--- app/src/main/java/com/example/cleveralarmclock/core/domain/usecase/schedule/RescheduleNextAlarmUseCase.kt
+++ app/src/main/java/com/example/cleveralarmclock/core/domain/usecase/schedule/RescheduleNextAlarmUseCase.kt
@@ -3,31 +3,33 @@ package com.example.cleveralarmclock.core.domain.usecase.schedule
 import com.example.cleveralarmclock.core.domain.alarm.AlarmSchedule
 import com.example.cleveralarmclock.core.domain.repository.AlarmRepository
 import com.example.cleveralarmclock.core.domain.usecase.manage.GetAlarmByIdUseCase
+import com.example.cleveralarmclock.core.domain.util.AlarmTimeCalculator
 import javax.inject.Inject
 
 class RescheduleNextAlarmUseCase @Inject constructor(
     private val getAlarmByIdUseCase: GetAlarmByIdUseCase,
-    private val alarmRepository: AlarmRepository,
-    private val alarmSchedule: AlarmSchedule
+    private val alarmSchedule: AlarmSchedule,
+    private val alarmRepository: AlarmRepository
 ){
-     suspend operator fun invoke(alarmId: Int, skipForToday: Boolean = false){
+     suspend operator fun invoke(alarmId: Int){
          val alarm = getAlarmByIdUseCase(alarmId) ?: return
 
          if(!alarm.isRepeated){
              val updatedAlarm = alarm.copy(isActivate = false)
              alarmRepository.updateAlarm(updatedAlarm)
+             alarmSchedule.cancel(alarmId)
+             return
          }
 
-         if(alarm.isRepeated){
-             alarmSchedule.schedule(
-                 alarm.hours,
-                 alarm.minutes,
-                 alarm.repeatDays,
-                 alarm.id,
-                 skipForToday
-             )
-         }else{
-             alarmSchedule.cancel(alarm.id)
-         }
+         val triggerTime = AlarmTimeCalculator.calculateNextTriggerTime(
+             alarm.hours,
+             alarm.minutes,
+             alarm.repeatDays,
+             false
+         )
+
+         alarmSchedule.schedule(alarm.id, triggerTime)
+
+
     }
 }
