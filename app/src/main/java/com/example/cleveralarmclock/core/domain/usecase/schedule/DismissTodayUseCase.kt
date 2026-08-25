82fe8dd890f6de8bef4c8e16d99ce8fc92a1f52b
Margarita <kkom.margarita@gmail.com>
2026-08-25 16:00:26 +0500

    refactor: update DismissTodayUseCase to use ScheduleAlarmUseCase and GetAlarmByIdUseCase

--- app/src/main/java/com/example/cleveralarmclock/core/domain/usecase/schedule/DismissTodayUseCase.kt
+++ app/src/main/java/com/example/cleveralarmclock/core/domain/usecase/schedule/DismissTodayUseCase.kt
@@ -1,13 +1,16 @@
 package com.example.cleveralarmclock.core.domain.usecase.schedule
 
+import com.example.cleveralarmclock.core.domain.usecase.manage.GetAlarmByIdUseCase
 import javax.inject.Inject
 
 class DismissTodayUseCase @Inject constructor(
-    private val rescheduleNextAlarmUseCase: RescheduleNextAlarmUseCase,
+    private val scheduleAlarmUseCase: ScheduleAlarmUseCase,
+    private val getAlarmByIdUseCase: GetAlarmByIdUseCase
 ) {
     suspend operator fun invoke(id: Int) {
+        val alarm = getAlarmByIdUseCase(id) ?: return
 
-        rescheduleNextAlarmUseCase(id, true)
+        scheduleAlarmUseCase(alarm, true)
 
     }
 }
