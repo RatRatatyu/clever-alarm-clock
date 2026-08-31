16d370f456c54053fd77aae5e3406a282b6f8672
Margarita <kkom.margarita@gmail.com>
2026-08-31 21:54:44 +0500

    feat: update AlarmList to pass lastDismissed property to alarm items

--- app/src/main/java/com/example/cleveralarmclock/core/data/database/entity/AlarmEntity.kt
+++ app/src/main/java/com/example/cleveralarmclock/core/data/database/entity/AlarmEntity.kt
@@ -2,6 +2,9 @@ package com.example.cleveralarmclock.core.data.database.entity
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import java.time.DayOfWeek
+import java.time.LocalDate
+import java.time.LocalDateTime
 
 @Entity(tableName = "alarmEntity")
 data class AlarmEntity (
@@ -10,7 +13,8 @@ data class AlarmEntity (
     val hours: Int,
     val minutes: Int,
     val taskId: String,
-    val repeatDays: String,
+    val repeatDays: Set<DayOfWeek>,
     val isRepeated: Boolean = true,
-    val isActivate: Boolean = true
+    val isActivate: Boolean = true,
+    val lastDismissed: LocalDate? = null
 )
