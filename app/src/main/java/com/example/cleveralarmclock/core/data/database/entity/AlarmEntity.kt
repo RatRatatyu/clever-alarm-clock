b05b36d95a7a8fac2ea467a7916c6a74fb24a119
Margarita <kkom.margarita@gmail.com>
2026-08-31 21:56:37 +0500

    refactor: remove unused DayOfWeek import in AlarmMapper

--- app/src/main/java/com/example/cleveralarmclock/core/data/database/entity/AlarmEntity.kt
+++ app/src/main/java/com/example/cleveralarmclock/core/data/database/entity/AlarmEntity.kt
@@ -4,7 +4,6 @@ import androidx.room.Entity
 import androidx.room.PrimaryKey
 import java.time.DayOfWeek
 import java.time.LocalDate
-import java.time.LocalDateTime
 
 @Entity(tableName = "alarmEntity")
 data class AlarmEntity (
