8f3e7852ae73135db690acb23d5946ffcb53213e
Margarita <kkom.margarita@gmail.com>
2026-08-31 21:53:06 +0500

    feat: update ScheduleAlarmUseCase to skip scheduling if alarm was dismissed today

--- app/src/main/java/com/example/cleveralarmclock/core/domain/usecase/schedule/ScheduleAlarmUseCase.kt
+++ app/src/main/java/com/example/cleveralarmclock/core/domain/usecase/schedule/ScheduleAlarmUseCase.kt
@@ -3,22 +3,27 @@ package com.example.cleveralarmclock.core.domain.usecase.schedule
 import com.example.cleveralarmclock.core.domain.alarm.AlarmSchedule
 import com.example.cleveralarmclock.core.domain.module.Alarm
 import com.example.cleveralarmclock.core.domain.util.AlarmTimeCalculator
+import java.time.LocalDate
 import javax.inject.Inject
 
 class ScheduleAlarmUseCase @Inject constructor(
     private val alarmSchedule: AlarmSchedule,
 ) {
     suspend operator fun invoke(alarm: Alarm, skipToday: Boolean = false) {
+        var isSkipped = skipToday
+
         if(!alarm.isActivate){
             alarmSchedule.cancel(alarm.id)
             return
         }
 
+        if (alarm.lastDismissed == LocalDate.now()) isSkipped = true
+
         val triggerTime = AlarmTimeCalculator.calculateNextTriggerTime(
             alarm.hours,
             alarm.minutes,
             alarm.repeatDays,
-            skipToday
+            isSkipped
         )
 
         alarmSchedule.schedule(alarm.id, triggerTime)
