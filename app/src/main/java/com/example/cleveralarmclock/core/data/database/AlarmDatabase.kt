aef228be8645b762b12313a5365a1c0369c9da01
Margarita <kkom.margarita@gmail.com>
2026-08-31 21:54:57 +0500

    feat: register AlarmConverters in AlarmDatabase

--- app/src/main/java/com/example/cleveralarmclock/core/data/database/AlarmDatabase.kt
+++ app/src/main/java/com/example/cleveralarmclock/core/data/database/AlarmDatabase.kt
@@ -4,11 +4,14 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import com.example.cleveralarmclock.core.data.database.converters.AlarmConverters
 import com.example.cleveralarmclock.core.data.database.dao.AlarmDao
 import com.example.cleveralarmclock.core.data.database.entity.AlarmEntity
 
 
 @Database(entities = [AlarmEntity::class], version = 2, exportSchema = false)
+@TypeConverters(AlarmConverters::class)
 abstract class AlarmDatabase: RoomDatabase() {
 
     abstract fun alarmDao(): AlarmDao
