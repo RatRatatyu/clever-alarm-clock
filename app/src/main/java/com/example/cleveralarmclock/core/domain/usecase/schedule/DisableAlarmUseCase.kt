04fb792f59f45f88aff7516ef8f84bc324fb1fc0
Margarita <kkom.margarita@gmail.com>
2026-08-31 21:54:05 +0500

    refactor: update DisableAlarmUseCase to reset lastDismissed and remove debug log

--- app/src/main/java/com/example/cleveralarmclock/core/domain/usecase/schedule/DisableAlarmUseCase.kt
+++ app/src/main/java/com/example/cleveralarmclock/core/domain/usecase/schedule/DisableAlarmUseCase.kt
@@ -1,6 +1,5 @@
 package com.example.cleveralarmclock.core.domain.usecase.schedule
 
-import android.util.Log
 import com.example.cleveralarmclock.core.domain.alarm.AlarmSchedule
 import com.example.cleveralarmclock.core.domain.repository.AlarmRepository
 import javax.inject.Inject
@@ -10,10 +9,12 @@ class DisableAlarmUseCase @Inject constructor(
     private val alarmSchedule: AlarmSchedule
 ) {
     suspend operator fun invoke(id: Int) {
-        Log.i("ALARM_DEBUG", "$id")
         val alarm = alarmRepository.getAlarmById(id) ?: return
 
-        val updatedAlarm = alarm.copy(isActivate = false)
+        val updatedAlarm = alarm.copy(
+            isActivate = false,
+            lastDismissed = null
+        )
 
         alarmRepository.updateAlarm(updatedAlarm)
         alarmSchedule.cancel(id)
