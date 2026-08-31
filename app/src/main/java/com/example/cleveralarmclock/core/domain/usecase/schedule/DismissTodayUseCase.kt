fa11d92622b9b750b31d6ba9c3ee452f01a07ff3
Margarita <kkom.margarita@gmail.com>
2026-08-31 21:53:48 +0500

    feat: update DismissTodayUseCase to persist last dismissed date

--- app/src/main/java/com/example/cleveralarmclock/core/domain/usecase/schedule/DismissTodayUseCase.kt
+++ app/src/main/java/com/example/cleveralarmclock/core/domain/usecase/schedule/DismissTodayUseCase.kt
@@ -1,16 +1,22 @@
 package com.example.cleveralarmclock.core.domain.usecase.schedule
 
+import com.example.cleveralarmclock.core.domain.repository.AlarmRepository
 import com.example.cleveralarmclock.core.domain.usecase.manage.GetAlarmByIdUseCase
+import java.time.LocalDate
 import javax.inject.Inject
 
 class DismissTodayUseCase @Inject constructor(
     private val scheduleAlarmUseCase: ScheduleAlarmUseCase,
-    private val getAlarmByIdUseCase: GetAlarmByIdUseCase
+    private val getAlarmByIdUseCase: GetAlarmByIdUseCase,
+    private val alarmRepository: AlarmRepository
 ) {
     suspend operator fun invoke(id: Int) {
-        val alarm = getAlarmByIdUseCase(id) ?: return
+       val alarm = getAlarmByIdUseCase(id) ?: return
 
-        scheduleAlarmUseCase(alarm, true)
+       val updatedAlarm =  alarm.copy(lastDismissed = LocalDate.now())
+
+       alarmRepository.updateAlarm(updatedAlarm)
+       scheduleAlarmUseCase(updatedAlarm, true)
 
     }
 }
