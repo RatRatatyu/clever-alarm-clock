0676a802beaa457ed7c9d8c5ca4f373f76c00320
Margarita <kkom.margarita@gmail.com>
2026-09-02 22:50:33 +0500

    refactor: remove skipToday parameter and simplify logic in ScheduleAlarmUseCase

--- app/src/main/java/com/example/cleveralarmclock/presentation/alarmAlertFeature/presentation/alarmRingFeature/AlarmRingScreen.kt
+++ app/src/main/java/com/example/cleveralarmclock/presentation/alarmAlertFeature/presentation/alarmRingFeature/AlarmRingScreen.kt
@@ -4,9 +4,11 @@ import android.app.Activity
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -16,15 +18,19 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.example.cleveralarmclock.R
 import com.example.cleveralarmclock.core.domain.task.TaskType
+import java.time.LocalDateTime
+import java.util.Locale
 
 
 @Composable
 fun AlarmRingScreen(
-    modifier: Modifier = Modifier,
     viewModel: AlarmRingViewModel = hiltViewModel(),
     onStopAlarm: (TaskType) -> Unit
 ) {
@@ -44,16 +50,34 @@ fun AlarmRingScreen(
 
     val uiState by viewModel.uiState.collectAsStateWithLifecycle()
 
+    AlarmRingComponent(
+        uiState = uiState,
+        snoozeAlarm = {viewModel.snoozeAlarm()},
+        onStopAlarm = {viewModel.onStopAlarm()}
+    )
+}
+
+
+
+@Composable
+fun AlarmRingComponent(
+    modifier: Modifier = Modifier,
+    uiState: AlarmRingUiState,
+    snoozeAlarm: () -> Unit,
+    onStopAlarm: () -> Unit
+){
     Scaffold(
         modifier = modifier
             .fillMaxSize()
     ) { innerPadding ->
 
+        val now = LocalDateTime.now()
+
         Column(
             modifier = Modifier
                 .fillMaxSize()
                 .padding(innerPadding),
-            verticalArrangement = Arrangement.Center,
+            verticalArrangement = Arrangement.SpaceBetween,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
 
@@ -61,13 +85,48 @@ fun AlarmRingScreen(
                 CircularProgressIndicator()
             }else{
 
-                Button(onClick = {viewModel.snoozeAlarm()}) {
-                    Text(stringResource(R.string.snoozing_for_10_minutes))
+                Column(
+                    modifier = Modifier
+                        .padding(top = 20.dp)
+                        .weight(1f),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Text(
+                        text = stringResource(R.string.alarm),
+                        style = MaterialTheme.typography.labelSmall,
+                        modifier = Modifier.padding(bottom = 10.dp)
+                    )
+
+                    Text(
+                        text = String.format(Locale.getDefault(), "%02d:%02d", now.hour, now.minute),
+                        style = MaterialTheme.typography.displayLarge.copy(fontSize = 100.sp),
+                    )
+
                 }
 
-                Button(onClick = { viewModel.onStopAlarm() }) {
-                    Text("Stop Alarm")
-                }
+               Column(
+                   modifier = Modifier
+                       .fillMaxWidth()
+                       .padding(horizontal = 30.dp)
+                       .weight(1f),
+                   horizontalAlignment = Alignment.CenterHorizontally,
+                   verticalArrangement = Arrangement.Center
+               ) {
+                   Button(
+                       onClick = { onStopAlarm() },
+                       modifier = Modifier.fillMaxWidth()
+                   ) {
+                       Text("Stop Alarm")
+                   }
+
+                   Button(
+                       onClick = {snoozeAlarm()},
+                       modifier = Modifier.fillMaxWidth()
+                   ) {
+                       Text(stringResource(R.string.snoozing_for_10_minutes))
+                   }
+               }
             }
         }
 
@@ -75,11 +134,17 @@ fun AlarmRingScreen(
 }
 
 
-
-
-
-
-
+@Preview
+@Composable
+fun AlarmRingScreenPreview(){
+    MaterialTheme{
+        AlarmRingComponent(
+            uiState = AlarmRingUiState(isLoading = false, taskId = TaskType.SHAKE),
+            onStopAlarm = {},
+            snoozeAlarm = {}
+        )
+    }
+}
 
 
 
