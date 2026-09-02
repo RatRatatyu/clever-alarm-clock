a08dc8cac73a300c695a89ecd2c00c8a1fabdb76
Margarita <kkom.margarita@gmail.com>
2026-09-02 22:50:54 +0500

    style: add blank line for readability in AlarmRingViewModel

--- app/src/main/java/com/example/cleveralarmclock/presentation/alarmAlertFeature/presentation/shakeTaskFeature/ShakeTaskViewModel.kt
+++ app/src/main/java/com/example/cleveralarmclock/presentation/alarmAlertFeature/presentation/shakeTaskFeature/ShakeTaskViewModel.kt
@@ -1,9 +1,11 @@
 package com.example.cleveralarmclock.presentation.alarmAlertFeature.presentation.shakeTaskFeature
 
+import android.util.Log
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.cleveralarmclock.core.domain.usecase.ring.StopAlarmPlayerUseCase
+import com.example.cleveralarmclock.core.service.sensors.ShakeDetector
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
 import javax.inject.Inject
@@ -13,17 +15,28 @@ import javax.inject.Inject
 class ShakeTaskViewModel @Inject constructor(
     savedStateHandle: SavedStateHandle,
     private val stopAlarmUseCase: StopAlarmPlayerUseCase,
+    private val shakeDetector: ShakeDetector
 ): ViewModel() {
 
     private val alarmId: Int = checkNotNull(savedStateHandle["alarmId"])
 
 
+    init {
+        shakeDetector.start()
+    }
 
     fun stopMusic(){
         viewModelScope.launch {
             stopAlarmUseCase(alarmId)
+            shakeDetector.stop()
         }
 
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        shakeDetector.stop()
+        Log.i("ALARM_DEBUG", "activity destroyed")
     }
 
 }
