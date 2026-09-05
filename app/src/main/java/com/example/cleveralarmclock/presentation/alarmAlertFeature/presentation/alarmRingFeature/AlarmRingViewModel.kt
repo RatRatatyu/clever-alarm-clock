5eba5e594a5e4fe39b55421e9d928b17c9be31bf
Margarita <kkom.margarita@gmail.com>
2026-09-05 22:54:54 +0500

    feat: add time-related properties to AlarmRingUiState and format current time in AlarmRingViewModel

--- app/src/main/java/com/example/cleveralarmclock/presentation/alarmAlertFeature/presentation/alarmRingFeature/AlarmRingViewModel.kt
+++ app/src/main/java/com/example/cleveralarmclock/presentation/alarmAlertFeature/presentation/alarmRingFeature/AlarmRingViewModel.kt
@@ -7,6 +7,7 @@ import com.example.cleveralarmclock.core.domain.task.AlarmTaskProvider
 import com.example.cleveralarmclock.core.domain.task.TaskType
 import com.example.cleveralarmclock.core.domain.usecase.manage.GetAlarmByIdUseCase
 import com.example.cleveralarmclock.core.domain.usecase.ring.SnoozeAlarmUseCase
+import com.example.cleveralarmclock.core.domain.util.DataTimeFormatter
 import dagger.hilt.android.lifecycle.HiltViewModel
 import jakarta.inject.Inject
 import kotlinx.coroutines.channels.Channel
@@ -16,10 +17,14 @@ import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.receiveAsFlow
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
+import java.time.LocalDateTime
 
 
 data class AlarmRingUiState(
     val isLoading: Boolean = false,
+    val currentTime: String = "00:00",
+    val amPmValue: String = "AM",
+    val is24Format: Boolean = true,
     val taskId: TaskType = TaskType.SHAKE,
     val taskName: Int? = null
 
@@ -29,7 +34,8 @@ data class AlarmRingUiState(
 class AlarmRingViewModel @Inject constructor(
     savedStateHandle: SavedStateHandle,
     private val getAlarmByIdUseCase: GetAlarmByIdUseCase,
-    private val snoozeAlarmUseCase: SnoozeAlarmUseCase
+    private val snoozeAlarmUseCase: SnoozeAlarmUseCase,
+    private val dataTimeFormatter: DataTimeFormatter
 ): ViewModel() {
 
     private val alarmId: Int = checkNotNull(savedStateHandle["alarmId"])
@@ -57,8 +63,17 @@ class AlarmRingViewModel @Inject constructor(
 
                     val alarm = getAlarmByIdUseCase(alarmId)
                     val taskName = AlarmTaskProvider.getTaskById(alarm?.taskId?.id ?: TaskType.SHAKE.id)
+                    val formatter = dataTimeFormatter.convert24To12Hour(LocalDateTime.now().hour)
+                    val timeFormatter = String.format(java.util.Locale.getDefault(), "%02d:%02d", formatter.hour,
+                        alarm?.minutes
+                    )
+
+
 
                     _uiState.update { it.copy(
+                        currentTime = timeFormatter,
+                        amPmValue = formatter.amPm,
+                        is24Format = formatter.is24Format,
                         taskId = alarm?.taskId ?: TaskType.SHAKE,
                         taskName = taskName?.titleResId
                     ) }
