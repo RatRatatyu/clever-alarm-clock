dcc60041b3984d78e319654a33a09ea237652e9f
Margarita <kkom.margarita@gmail.com>
2026-09-05 22:55:05 +0500

    style: update icon and title resources for SHAKE and CAMERA tasks in AlarmTaskProvider

--- app/src/main/java/com/example/cleveralarmclock/core/domain/task/AlarmTaskProvider.kt
+++ app/src/main/java/com/example/cleveralarmclock/core/domain/task/AlarmTaskProvider.kt
@@ -6,15 +6,15 @@ object AlarmTaskProvider{
     val allTasks = listOf(
         AlarmTask(
             type = TaskType.SHAKE,
-            titleResId = R.string.shake,
+            titleResId = R.string.shake_task,
             descriptionResId = R.string.shake_your_phone_to_turn_the_alarm_off,
-            iconResId = android.R.drawable.ic_menu_preferences // will be changed in the future
+            iconResId = R.drawable.outline_mobile_vibrate
         ),
         AlarmTask(
             type = TaskType.CAMERA,
             titleResId = R.string.ai_task,
             descriptionResId = R.string.take_a_photo_of_required_object_to_turn_the_alarm_off,
-            iconResId = android.R.drawable.ic_menu_camera // will be changed in the future
+            iconResId = R.drawable.outline_photo_camera
         )
     )
 
