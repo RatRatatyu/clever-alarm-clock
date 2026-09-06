aad22db4e42bdb497758cc228f4b2b3c05f771a5
Margarita <kkom.margarita@gmail.com>
2026-09-06 15:52:13 +0500

    style: update ShakeTaskScreen to use string resources for task status labels and content description

--- app/src/main/java/com/example/cleveralarmclock/presentation/alarmAlertFeature/presentation/shakeTaskFeature/ShakeTaskScreen.kt
+++ app/src/main/java/com/example/cleveralarmclock/presentation/alarmAlertFeature/presentation/shakeTaskFeature/ShakeTaskScreen.kt
@@ -20,6 +20,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -94,7 +95,7 @@ fun ShakeTaskComponent(
                             painter = painterResource(
                                 id = if(isTaskFinished) R.drawable.outline_mobile_check else R.drawable.outline_mobile_vibrate
                             ),
-                            contentDescription = "Shake phone",
+                            contentDescription = stringResource(R.string.shake_phone),
                             modifier = Modifier
                                 .size(90.dp)
                                 .padding(vertical = 10.dp),
@@ -102,7 +103,9 @@ fun ShakeTaskComponent(
                         )
 
                         Text(
-                            text = if(isTaskFinished) "Well done!" else "Shake your phone!",
+                            text = if(isTaskFinished) stringResource(R.string.well_done) else stringResource(
+                                R.string.shake_your_phone
+                            ),
                             style = MaterialTheme.typography.titleLarge,
                             color = MaterialTheme.colorScheme.onPrimary,
                         )
