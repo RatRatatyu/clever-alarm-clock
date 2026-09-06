947129f941d1dadea52e3a0d79362667690fcea6
Margarita <kkom.margarita@gmail.com>
2026-09-06 15:52:05 +0500

    style: refactor CameraTaskScreen into component, add Scaffold, and provide dark theme preview

--- app/src/main/java/com/example/cleveralarmclock/presentation/alarmAlertFeature/presentation/cameraTaskFeature/CameraTaskScreen.kt
+++ app/src/main/java/com/example/cleveralarmclock/presentation/alarmAlertFeature/presentation/cameraTaskFeature/CameraTaskScreen.kt
@@ -3,25 +3,57 @@ package com.example.cleveralarmclock.presentation.alarmAlertFeature.presentation
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.darkColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.hilt.navigation.compose.hiltViewModel
 
 @Composable
 fun CameraTaskScreen(
-    modifier: Modifier = Modifier,
     viewModel: CameraTaskViewModel = hiltViewModel()
 ){
-    Column(
-        modifier
-            .fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
-    ) {
-        Text("This is screen task for CAMERA")
-        Button(onClick = { viewModel.stopMusic() }) { Text("stop") }
+    CameraTaskComponent(stopMusic = {viewModel.stopMusic()})
+}
+
+
+@Composable
+fun CameraTaskComponent(
+    modifier: Modifier = Modifier,
+    stopMusic: () -> Unit
+){
+
+    Scaffold(
+        modifier.fillMaxSize()
+    ) { innerPadding ->
+        Column(
+            Modifier
+                .fillMaxSize()
+                .padding(innerPadding),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Text("This is screen task for CAMERA")
+            Button(onClick = { stopMusic() }) { Text("stop") }
+        }
+
+    }
+
+}
+
+
+@Preview
+@Composable
+fun CameraTaskPreview(){
+    MaterialTheme(darkColorScheme()){
+        CameraTaskComponent(
+            stopMusic = {}
+        )
     }
 }
