91ddce914aa65f18a3da4c4dc6aa97a9651c8e10
Margarita <kkom.margarita@gmail.com>
2026-09-06 20:02:32 +0500

    build: add CameraX view, camera2, and lifecycle dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,6 +43,9 @@ android {
 
 
 dependencies {
+    implementation(libs.androidx.camera.view)
+    implementation(libs.androidx.camera.camera2)
+    implementation(libs.androidx.camera.lifecycle)
     implementation(libs.androidx.compose.material3.windowSizeClass)
     implementation(libs.androidx.lifecycle.service)
     implementation(libs.androidx.room.runtime)
