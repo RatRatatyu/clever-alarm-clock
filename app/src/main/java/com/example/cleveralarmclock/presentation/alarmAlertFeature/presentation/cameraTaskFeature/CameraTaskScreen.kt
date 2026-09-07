9f91ff86cbbf64717db3fcbbacb416d1eba3c629
Margarita <kkom.margarita@gmail.com>
2026-09-07 16:15:46 +0500

    style: remove unused Button and theme imports in CameraTaskScreen

--- app/src/main/java/com/example/cleveralarmclock/presentation/alarmAlertFeature/presentation/cameraTaskFeature/CameraTaskScreen.kt
+++ app/src/main/java/com/example/cleveralarmclock/presentation/alarmAlertFeature/presentation/cameraTaskFeature/CameraTaskScreen.kt
@@ -5,7 +5,6 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
@@ -21,7 +20,6 @@ import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.example.cleveralarmclock.presentation.alarmAlertFeature.presentation.cameraTaskFeature.components.CameraViewComponent
-import com.example.cleveralarmclock.ui.theme.CleverAlarmClockTheme
 
 @Composable
 fun CameraTaskScreen(
